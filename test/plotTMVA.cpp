0234fa7e03c2891a62627787d91105f358005e3f
Pietro <pietro.govoni@cern.ch>
2015-07-24 11:50:31 +0200

    added info in the doc session, plus work ongoing

---
+++ test/plotTMVA.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <cmath>
+#include <cctype>
+#include <algorithm>
+#include "ConfigParser.h"
+#include "utils.h"
+#include "TString.h"
+#include "TChain.h"
+#include "TCut.h"
+#include "HistoManager.h"
+#include "TTreeFormula.h"
+#include "THStack.h"
+#include "TCanvas.h"
+
+#include "histoUtils.h"
+
+using namespace std ;
+
+int main (int argc, char** argv)
+{
+  // check number of inpt parameters
+  if (argc < 2)
+    {
+      cerr << "Forgot to put the cfg file --> exit " << endl ;
+      return 1 ;
+    }
+  if (gConfigParser) return 1 ;
+  gConfigParser = new ConfigParser () ;
+  
+  TString config ; 
+  config.Form ("%s",argv[1]) ;
+  if (! (gConfigParser->init (config)))
+    {
+      cout << ">>> parseConfigFile::Could not open configuration file " << config << endl ;
+      return -1 ;
+    }
+
+  float lumi = gConfigParser->readFloatOption ("general::lumi") ;
+  cout << "READING lumi " << lumi << endl ;
+  
+  // get the samples to be analised
+  // ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
+  
+  vector<string> sigSamplesList = gConfigParser->readStringListOption ("general::signals") ;
+  vector<sample> sigSamples ;
+  readSamples (sigSamples, sigSamplesList) ;
+
+  vector<float> signalScales ;
+  for (unsigned int i = 0 ; i < sigSamplesList.size () ; ++i)
+    {
+      string name = string ("samples::") + sigSamplesList.at (i) + string ("FACT") ;
+      float scale = gConfigParser->readFloatOption (name.c_str ()) ;
+      signalScales.push_back (scale) ;        
+    }
+
+  vector<string> bkgSamplesList = gConfigParser->readStringListOption ("general::backgrounds") ;
+  vector<sample> bkgSamples ;
+  readSamples (bkgSamples, bkgSamplesList) ;
+
+  vector<string> DATASamplesList = gConfigParser->readStringListOption ("general::data") ;
+  vector<sample> DATASamples ;
+  readSamples (DATASamples, DATASamplesList) ;
+
+  // get the selections to be applied
+  // ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
+
+  vector<pair <TString, TCut> > selections = readCutsFile (
+      gConfigParser->readStringOption ("selections::selectionsFile")
+    ) ;
+
+  cout << "\n-====-====-====-====-====-====-====-====-====-====-====-====-====-\n\n" ;
+  cout << "selections sequence: \n" ;
+  for (unsigned int i = 0 ; i < selections.size () ; ++i)
+    cout << selections.at (i).first << " : " << selections.at (i).second << endl ;
+  cout << "\n-====-====-====-====-====-====-====-====-====-====-====-====-====-\n\n" ;
+
+  // get the variables to be plotted
+  // ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
+
+  vector<string> variablesList = gConfigParser->readStringListOption ("general::variables") ;
+
+  TString histoName ;
+  HistoManager * manager = new HistoManager ("test") ;
+
+  return 0 ;
+
+}
