cd443869e8b95ae20e29543863e2731645ac22e2
Pietro <pietro.govoni@cern.ch>
2015-07-27 14:53:44 +0200

    bugfix

--- test/plotter.cpp
+++ test/plotter.cpp
@@ -15,6 +15,7 @@
 #include "TCanvas.h"
 
 #include "histoUtils.h"
+#include "utils.h"
 
 using namespace std ;
 
@@ -44,50 +45,6 @@ using namespace std ;
 https://github.com/govoni/FlatNtStudy/blob/master/interface/plotter.h#L324
 */
 
-// xmin ymin xmax ymax
-vector<float> getExtremes (THStack * hstack, bool islog = false)
-{
-  float ymax = hstack->GetMaximum () * 1.3;
-
-  TIter next (hstack->GetHists ()) ;
-  TH1F * histo ;
-
-  float xmin = 1. ;
-  float xmax = 0. ;
-  float ymin = 10000000000. ;
-  while (histo = (TH1F *) (next ())) 
-    {
-      float tmpmin = findNonNullMinimum (histo) ;
-      if (tmpmin < ymin) ymin = tmpmin ;
-      if (xmin > xmax)
-        {
-          xmin = histo->GetXaxis ()->GetXmin () ;
-          xmax = histo->GetXaxis ()->GetXmax () ;
-        }
-    }
-
-  ymin *= 0.9 ;
-
-  vector<float> extremes (4, 0.) ;
-  extremes.at (0) = xmin ;
-  extremes.at (1) = ymin ;
-  extremes.at (2) = xmax ;
-  extremes.at (3) = ymax ;
-
-  return extremes ;
-}
-
-
-// --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-
-
-struct isNOTalnum : std::unary_function<int, int>
-{
-    int operator()(int i) const { return !std::isalnum (i) ; }
-};
-
-
-// --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
 
 int main (int argc, char** argv)
