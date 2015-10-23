79afa70ce6b159d56ba08404096c8113cc30f9d3
Giacomo Ortona <gortona@cern.ch>
2015-10-23 10:46:33 +0200

    big fix

--- test/evalQCD.cpp
+++ test/evalQCD.cpp
@@ -243,7 +243,7 @@ int main (int argc, char** argv)
               variablesList, variables2DList,
               selections_OS,
               lumi,
-              vector<float> (0),
+              signalScales,
               false, true) ;
   OS_sig_plots.AddOverAndUnderFlow () ;
 
