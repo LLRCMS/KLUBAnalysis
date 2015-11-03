e3645fb08b53466aece1da14fd3b36eb621690d9
Luca Cadamuro <lc.cadamuro@gmail.com>
2015-11-03 17:59:17 +0100

    update skim script

--- scripts/submit_skims.sh
+++ scripts/submit_skims.sh
@@ -24,23 +24,23 @@
 # python scripts/skimNtuple.py -s True -c  config/skim.cfg  -n 50 -k False -o /data_CMS/cms/cadamuro/test_submit_to_tier3/Skims10_AllJets_NI/SKIM_HHLambda2dot46_New    -i inputFiles/13Ott2015//HH_Lambda2dot46.txt  -x 0.0144  
 
 # Run D data 25 ns --> un po' meno di 1.5/fb (da controllare vista la mancanza dei dati run C)
-python scripts/skimNtuple.py -d True  -s True -c  config/skim.cfg  -n 70 -k True -o /data_CMS/cms/cadamuro/test_submit_to_tier3/Skims14_AllJets_NI/SKIM_SingleMuon_31OttD_PromptReco        -i inputFiles/31Ott2015_noSVFit/merged/SingleMuon_RunD.txt
-python scripts/skimNtuple.py -d True  -s True -c  config/skim.cfg  -n 70 -k True -o /data_CMS/cms/cadamuro/test_submit_to_tier3/Skims14_AllJets_NI/SKIM_Tau_31OttD_PromptReco               -i inputFiles/31Ott2015_noSVFit/merged/Tau_RunD.txt
-python scripts/skimNtuple.py -d True  -s True -c  config/skim.cfg  -n 70 -k True -o /data_CMS/cms/cadamuro/test_submit_to_tier3/Skims14_AllJets_NI/SKIM_SingleElectron_31OttD_PromptReco    -i inputFiles/31Ott2015_noSVFit/merged/SingleElectron_RunD.txt
+python scripts/skimNtuple.py -d True  -s True -c  config/skim.cfg  -n 30 -k True -o /data_CMS/cms/cadamuro/test_submit_to_tier3/Skims14_AllJets_NI/SKIM_SingleMuon_31OttD_PromptReco        -i inputFiles/31Ott2015_noSVFit/merged/SingleMuon_RunD.txt
+python scripts/skimNtuple.py -d True  -s True -c  config/skim.cfg  -n 30 -k True -o /data_CMS/cms/cadamuro/test_submit_to_tier3/Skims14_AllJets_NI/SKIM_Tau_31OttD_PromptReco               -i inputFiles/31Ott2015_noSVFit/merged/Tau_RunD.txt
+python scripts/skimNtuple.py -d True  -s True -c  config/skim.cfg  -n 30 -k True -o /data_CMS/cms/cadamuro/test_submit_to_tier3/Skims14_AllJets_NI/SKIM_SingleElectron_31OttD_PromptReco    -i inputFiles/31Ott2015_noSVFit/merged/SingleElectron_RunD.txt
 
 # HH Signals
-python scripts/skimNtuple.py -s True -c  config/skim.cfg  -n 50 -k False -o /data_CMS/cms/cadamuro/test_submit_to_tier3/Skims14_AllJets_NI/SKIM_HHLambda1_New         -i inputFiles/28Ago2015/HH_Lambda1.txt       -x 0.00247
-python scripts/skimNtuple.py -s True -c  config/skim.cfg  -n 50 -k False -o /data_CMS/cms/cadamuro/test_submit_to_tier3/Skims14_AllJets_NI/SKIM_HHLambda20_New        -i inputFiles/28Ago2015/HH_Lambda20.txt      -x 0.259 
-python scripts/skimNtuple.py -s True -c  config/skim.cfg  -n 50 -k False -o /data_CMS/cms/cadamuro/test_submit_to_tier3/Skims14_AllJets_NI/SKIM_HHLambdam4_New        -i inputFiles/28Ago2015/HH_Lambdam4.txt      -x 0.0296  
-python scripts/skimNtuple.py -s True -c  config/skim.cfg  -n 50 -k False -o /data_CMS/cms/cadamuro/test_submit_to_tier3/Skims14_AllJets_NI/SKIM_HHLambda2dot46_New    -i inputFiles/28Ago2015/HH_Lambda2dot46.txt  -x 0.00104  
+python scripts/skimNtuple.py -s True -c  config/skim.cfg  -n 30 -k True -o /data_CMS/cms/cadamuro/test_submit_to_tier3/Skims14_AllJets_NI/SKIM_HHLambda1_New         -i inputFiles/28Ago2015/HH_Lambda1.txt       -x 0.00247
+python scripts/skimNtuple.py -s True -c  config/skim.cfg  -n 30 -k True -o /data_CMS/cms/cadamuro/test_submit_to_tier3/Skims14_AllJets_NI/SKIM_HHLambda20_New        -i inputFiles/28Ago2015/HH_Lambda20.txt      -x 0.259 
+python scripts/skimNtuple.py -s True -c  config/skim.cfg  -n 30 -k True -o /data_CMS/cms/cadamuro/test_submit_to_tier3/Skims14_AllJets_NI/SKIM_HHLambdam4_New        -i inputFiles/28Ago2015/HH_Lambdam4.txt      -x 0.0296  
+python scripts/skimNtuple.py -s True -c  config/skim.cfg  -n 30 -k True -o /data_CMS/cms/cadamuro/test_submit_to_tier3/Skims14_AllJets_NI/SKIM_HHLambda2dot46_New    -i inputFiles/28Ago2015/HH_Lambda2dot46.txt  -x 0.00104  
 
 # MC backgrounds
-python scripts/skimNtuple.py -s True -c  config/skim.cfg  -n 50 -k False -o /data_CMS/cms/cadamuro/test_submit_to_tier3/Skims14_AllJets_NI/SKIM_WW_TuneCUETP8M1 -i inputFiles/28Ago2015/6_WW_TuneCUETP8M1_13TeV-pythia8__RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1.txt     -x 63.21
-python scripts/skimNtuple.py -s True -c  config/skim.cfg  -n 50 -k False -o /data_CMS/cms/cadamuro/test_submit_to_tier3/Skims14_AllJets_NI/SKIM_WZ_TuneCUETP8M1 -i inputFiles/28Ago2015/7_WZ_TuneCUETP8M1_13TeV-pythia8__RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1.txt     -x 22.82
-python scripts/skimNtuple.py -s True -c  config/skim.cfg  -n 50 -k False -o /data_CMS/cms/cadamuro/test_submit_to_tier3/Skims14_AllJets_NI/SKIM_ZZ_TuneCUETP8M1 -i inputFiles/28Ago2015/8_ZZ_TuneCUETP8M1_13TeV-pythia8__RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v3.txt     -x 10.32
-python scripts/skimNtuple.py -s True -c  config/skim.cfg  -n 50 -k False -o /data_CMS/cms/cadamuro/test_submit_to_tier3/Skims14_AllJets_NI/SKIM_TTJets          -i inputFiles/28Ago2015/5_TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8__RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1.txt              -x 831.76 
-python scripts/skimNtuple.py -s True -c  config/skim.cfg  -n 50 -k False -o /data_CMS/cms/cadamuro/test_submit_to_tier3/Skims14_AllJets_NI/SKIM_WJet            -i inputFiles/28Ago2015/1_WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8__RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1.txt          -x 61526.7 
-python scripts/skimNtuple.py -s True -c  config/skim.cfg  -n 50 -k False -o /data_CMS/cms/cadamuro/test_submit_to_tier3/Skims14_AllJets_NI/SKIM_DY              -i inputFiles/28Ago2015/2_DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8__RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v3.txt     -x 6025.2 
+python scripts/skimNtuple.py -s True -c  config/skim.cfg  -n 30 -k True -o /data_CMS/cms/cadamuro/test_submit_to_tier3/Skims14_AllJets_NI/SKIM_WW_TuneCUETP8M1 -i inputFiles/28Ago2015/6_WW_TuneCUETP8M1_13TeV-pythia8__RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1.txt     -x 63.21
+python scripts/skimNtuple.py -s True -c  config/skim.cfg  -n 30 -k True -o /data_CMS/cms/cadamuro/test_submit_to_tier3/Skims14_AllJets_NI/SKIM_WZ_TuneCUETP8M1 -i inputFiles/28Ago2015/7_WZ_TuneCUETP8M1_13TeV-pythia8__RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1.txt     -x 22.82
+python scripts/skimNtuple.py -s True -c  config/skim.cfg  -n 30 -k True -o /data_CMS/cms/cadamuro/test_submit_to_tier3/Skims14_AllJets_NI/SKIM_ZZ_TuneCUETP8M1 -i inputFiles/28Ago2015/8_ZZ_TuneCUETP8M1_13TeV-pythia8__RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v3.txt     -x 10.32
+python scripts/skimNtuple.py -s True -c  config/skim.cfg  -n 60 -k True -o /data_CMS/cms/cadamuro/test_submit_to_tier3/Skims14_AllJets_NI/SKIM_TTJets          -i inputFiles/28Ago2015/5_TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8__RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1.txt              -x 831.76 
+python scripts/skimNtuple.py -s True -c  config/skim.cfg  -n 60 -k True -o /data_CMS/cms/cadamuro/test_submit_to_tier3/Skims14_AllJets_NI/SKIM_WJet            -i inputFiles/28Ago2015/1_WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8__RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1.txt          -x 61526.7 
+python scripts/skimNtuple.py -s True -c  config/skim.cfg  -n 60 -k True -o /data_CMS/cms/cadamuro/test_submit_to_tier3/Skims14_AllJets_NI/SKIM_DY              -i inputFiles/28Ago2015/2_DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8__RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v3.txt     -x 6025.2 
 
 
 # TODO: single top, t-W, antit-W
