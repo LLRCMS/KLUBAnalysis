e2c5108b479096283078d609d51315a72939f5d9
Luca Cadamuro <lc.cadamuro@gmail.com>
2016-01-11 12:13:37 +0100

    lepton scale factors

---
+++ interface/ScaleFactor.h
@@ -0,0 +1,41 @@
+#ifndef ScaleFactor_h
+#define ScaleFactor_h
+
+#include "TROOT.h"
+#include "TFile.h"
+#include "TH1.h"
+#include "TGraphAsymmErrors.h"
+#include <iostream>
+#include <map>
+#include <cmath>
+#include <string>
+
+
+
+
+class ScaleFactor {
+
+	private: 
+	std::map<std::string, TGraphAsymmErrors *> eff_data;
+	std::map<std::string, TGraphAsymmErrors *> eff_mc;
+
+	TH1D * etaBinsH;
+
+	void  SetAxisBins(TGraphAsymmErrors*);
+	bool  check_SameBinning(TGraphAsymmErrors*, TGraphAsymmErrors*);
+
+	public:
+		ScaleFactor(){}; 
+		void init_ScaleFactor(TString);
+		void init_ScaleFactor(TString,std::string);
+		~ ScaleFactor(){};
+		double get_EfficiencyData(double, double); //pt, eta
+		double get_EfficiencyMC(double, double);
+		double get_ScaleFactor(double, double); 
+	
+};
+
+
+#endif
+
+
