abd53e35c079355b10f31df55494385e0fdd2a30
Luca Cadamuro <lc.cadamuro@gmail.com>
2016-05-23 18:55:30 +0200

    safe setup to avoid overriding configurations

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,23 @@
-export THISDIR=`pwd`
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${THISDIR}/lib
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${THISDIR}/../HHKinFit2
+paths=$(echo $PATH | tr ":" "\n") # split on :
+pathunset=true
+for pp in $paths
+do
+    if [[ $pp == *"KLUBAnalysis/bin"* ]] ; then
+        echo "Warning: path to KLUB binaries already set to: $pp"
+        echo "         ... Ignoring this setup command"
+        pathunset=false
+    fi
+done
 
-if [ -n "${DYLD_LIBRARY_PATH}" ] ; then
-export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:${THISDIR}/lib
-export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:${THISDIR}/../HHKinFit2
+if [ "$pathunset" = true ] ; then
+    export THISDIR=`pwd`
+    export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${THISDIR}/lib
+    export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${THISDIR}/../HHKinFit2
+
+    if [ -n "${DYLD_LIBRARY_PATH}" ] ; then
+    export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:${THISDIR}/lib
+    export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:${THISDIR}/../HHKinFit2
+    fi
+
+    export PATH=${PATH}:${THISDIR}/bin
 fi
-
-export PATH=${PATH}:${THISDIR}/bin
