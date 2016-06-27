11f57c45a2d29cd0d2563b508e679ce944c85770
Luca Cadamuro <lc.cadamuro@gmail.com>
2016-06-27 21:20:41 +0200

    check file intergrity in hadd

--- scripts/haddAll.sh
+++ scripts/haddAll.sh
@@ -7,6 +7,18 @@ if [ $HASHADD = true ] ; then
        cd $dir
        pwd
        if [ ! -f total.root ]; then # safe if already run on part of dataset
+           mkdir corrupted
+           for logfile in output_*.log
+           do
+               tmp=${logfile#*_}
+               idx=${tmp%.*}
+               if ! grep -q "... SKIM finished, exiting." "$logfile"; then
+                  echo "job num $idx not correctly finished"
+                  mv "output_$idx.root" corrupted
+                  mv $logfile corrupted
+               fi
+           done           
+
            hadd total.root output_*.root
            mkdir singleFiles
            mv output_*.root singleFiles
