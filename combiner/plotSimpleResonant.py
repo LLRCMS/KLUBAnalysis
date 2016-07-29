846a4b44ab3b67ec872d790eeaf0c65d07fabf24
Luca Cadamuro <lc.cadamuro@gmail.com>
2016-07-29 11:57:05 +0200

    style of limit plots resonant

--- combiner/plotSimpleResonant.py
+++ combiner/plotSimpleResonant.py
@@ -56,7 +56,7 @@ for i in channels :
 			#if i == "COMBINED" :
 			#	fin = TFile.Open("cards_"+i+"_"+folder+"/Radion"+str(m)+"COMBINED"+c+"/higgsCombineRadion"+str(m)+"_forLim.Asymptotic.mH125.root")
 			#else :
-			fin = TFile.Open("cards_"+name+"_"+folder+"/Radion"+str(m)+c+"HHKin_mass_raw/higgsCombineRadion"+str(m)+"_forLim.Asymptotic.mH125.root")
+			fin = TFile.Open("/home/llr/cms/ortona/diHiggs/CMSSW_7_4_7/src/KLUBAnalysis/combiner/cards_"+name+"_"+folder+"/Radion"+str(m)+c+"HHKin_mass_raw/higgsCombineRadion"+str(m)+"_forLim.Asymptotic.mH125.root")
 			tree = fin.Get("limit")
 			high=0
 			low=0
@@ -89,10 +89,11 @@ for c in range(len(channels)) :
 	gObs.SetName("Observed")
 	gObs.SetLineColor(1)
 	gObs.SetMarkerColor(1)
-	gObs.SetMarkerStyle(20)
+	gObs.SetLineWidth(2)
+	gObs.SetMarkerStyle(8)
 	gObs.SetFillStyle(0)
 	for m in masses:
-		fin = TFile.Open("cards_"+channels[c]+"_"+folder+"/Radion"+str(m)+"/higgsCombineRadion"+str(m)+"_forLim.Asymptotic.mH125.root")
+		fin = TFile.Open("/home/llr/cms/ortona/diHiggs/CMSSW_7_4_7/src/KLUBAnalysis/combiner/cards_"+channels[c]+"_"+folder+"/Radion"+str(m)+"/higgsCombineRadion"+str(m)+"_forLim.Asymptotic.mH125.root")
 		tree = fin.Get("limit")
 		high=0
 		low=0
@@ -137,35 +138,61 @@ for c in range(len(channels)) :
 #	c[ic].SetLogy()
 #	c[ic].SaveAs("limitResonant_"+channels[ic]+outString+".pdf")
 
-cNice = [TCanvas("ETauFinal"),TCanvas("MuTauFinal"),TCanvas("TauTauFinal"),TCanvas("COMBINEDFinal")]
+cNice = [
+	TCanvas("ETauFinal", "ETauFinal", 650, 500),
+	TCanvas("MuTauFinal", "MuTauFinal", 650, 500),
+	TCanvas("TauTauFinal", "TauTauFinal", 650, 500),
+	TCanvas("COMBINEDFinal", "COMBINEDFinal", 650, 500)
+	]
+
 for ic in range(len(channels)):
 	cNice[ic].cd()
+	cNice[ic].SetFrameLineWidth(3)
+	cNice[ic].SetBottomMargin (0.15)
+	cNice[ic].SetRightMargin (0.05)
+	cNice[ic].SetLeftMargin (0.15)
+
 	#mg[ic].GetListOfGraphs().ls()
 	gexp = mg[ic].GetListOfGraphs().FindObject("Combined categories").Clone()
-	gexp.SetTitle("Expected CLs, all categories")
+	gexp.SetTitle("Expected CLs")
 	gexp.SetMarkerStyle(24)
 	gexp.SetMarkerColor(4)
-	gexp.SetLineColor(4)
+	gexp.SetMarkerSize(0.8)
+	gexp.SetLineColor(kBlue+2)
 	gexp.SetLineStyle(2)
+	gexp.SetLineWidth(2)
 	gexp.SetFillColor(0)
 	g68 = mg[ic].GetListOfGraphs().FindObject("Combined categories").Clone()
 	g68.SetTitle("Expected #pm 1#sigma")
 	g68.SetMarkerStyle(0)
 	g68.SetMarkerColor(3)
-	g68.SetFillColor(3)
-	g68.SetLineColor(3)
+	# colG = TColor()
+	# colG.SetRGB(0, 255, 0); #r, g, b
+	g68.SetFillColor(TColor.GetColor(0, 220, 60))
+	g68.SetLineColor(TColor.GetColor(0, 220, 60))
 	g68.SetFillStyle(1001)
 	g95[ic].SetTitle("Expected #pm 2#sigma")
 	g95[ic].SetName(channels[ic])
 	g95[ic].SetMarkerStyle(0)
 	g95[ic].SetMarkerColor(5)
-	g95[ic].SetFillColor(5)
-	g95[ic].SetLineColor(5)
+	g95[ic].SetFillColor(TColor.GetColor(254, 234, 0))
+	g95[ic].SetLineColor(TColor.GetColor(254, 234, 0))
 	g95[ic].SetFillStyle(1001)
 	g95[ic].GetYaxis().SetRangeUser(0.01*scale,1000*scale)
 	g95[ic].GetXaxis().SetRangeUser(250,900)
-	g95[ic].GetYaxis().SetTitle("95% CL on #sigma#times BR(H#rightarrow H#rightarrow hh#rightarrow bb#tau#tau) [pb]")
+	g95[ic].GetYaxis().SetTitle("95% CL on #sigma#times BR(H#rightarrow hh#rightarrow bb#tau#tau) [pb]")
 	g95[ic].GetXaxis().SetTitle("m_{H} [GeV]")
+
+	g95[ic].GetYaxis().SetTitleSize(0.047)
+	g95[ic].GetXaxis().SetTitleSize(0.055)
+	g95[ic].GetYaxis().SetLabelSize(0.045)
+	g95[ic].GetXaxis().SetLabelSize(0.045)
+	g95[ic].GetXaxis().SetLabelOffset(0.012)
+
+	g95[ic].GetYaxis().SetTitleOffset(1.15)
+	g95[ic].GetXaxis().SetTitleOffset(1.1)
+
+
 	gStyle.SetOptTitle(0)
 	g95[ic].Draw("A3")
 	g68.Draw("3SAME")
@@ -175,21 +202,22 @@ for ic in range(len(channels)):
 		gObs.Draw("PLSAME")
 
 	legend = TLegend()#	cNice[ic].BuildLegend()
+	legend.SetFillStyle(0)
 	legend.SetLineColor(0)
-	legend.SetX1(0.1192529)
-	legend.SetY1(0.1292373)
-	legend.SetX2(0.4755)
-	legend.SetY2(0.2733)
+	legend.SetX1(0.164)
+	legend.SetY1(0.1992373)
+	legend.SetX2(0.56)
+	legend.SetY2(0.39)
 	if addObserved : legend.AddEntry(gObs,"Observed","PL")
-	legend.AddEntry(gexp)
-	legend.AddEntry(g68)
-	legend.AddEntry(g95[ic])
+	legend.AddEntry(gexp, gexp.GetTitle(), "lp")
+	legend.AddEntry(g68, g68.GetTitle(), "f")
+	legend.AddEntry(g95[ic], g95[ic].GetTitle(), "f")
 	legend.Draw()
 	cNice[ic].SetLogy()
 	cNice[ic].SetGridy(1)
 	cNice[ic].SetGridx(1)
 
-	pt = TPaveText(0.1063218,0.7966102,0.3045977,0.8898305,"brNDC")
+	pt = TPaveText(0.1663218,0.7966102,0.3045977,0.8898305,"brNDC")
 	pt.SetBorderSize(0)
 	pt.SetTextAlign(12)
 	pt.SetTextFont(62)
@@ -198,22 +226,24 @@ for ic in range(len(channels)):
 	pt.SetFillStyle(0)
 	pt.AddText("CMS" )
 	pt.AddText("#font[52]{preliminary}")
-	pt2 = TPaveText(0.7,0.9066667,0.8997773,0.957037,"brNDC")
+	pt2 = TPaveText(0.79,0.9066667,0.8997773,0.957037,"brNDC")
 	pt2.SetBorderSize(0)
 	pt2.SetFillColor(0)
-	pt2.SetTextSize(0.035)
+	pt2.SetTextSize(0.040)
 	pt2.SetTextFont(42)
 	pt2.SetFillStyle(0)
-	pt2.AddText("L = 12.6 fb^{-1} (13 TeV)")
+	pt2.AddText("12.9 fb^{-1} (13 TeV)")
 
-	pt4 = TPaveText(0.3819196,0.7780357,0.9008929,0.8675595,"brNDC")
+	pt4 = TPaveText(0.4819196,0.7780357,0.9008929,0.8675595,"brNDC")
 	pt4.SetTextAlign(12)
 	pt4.SetFillColor(0)
 	pt4.SetFillStyle(0)
 	pt4.SetTextFont(42)
+	pt4.SetTextSize(0.05)
 	pt4.SetBorderSize(0)
+	pt4.SetTextAlign(32)
 	pt4.AddText(channelsName[ic]) 
-	if ic == 3 : pt4.AddText("Combined channels")
+	if ic == 3 : pt4.AddText("#scale[0.8]{Combined channels}")
 	pt.Draw()
 	pt2.Draw()
 	pt4.Draw()
