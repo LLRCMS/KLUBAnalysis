7c20409711b118f37af5fc106f021a26fcddeb29
camendola <chia.amendola@gmail.com>
2016-11-30 11:46:57 +0100

    eff plots and multigraph

--- makeEffPlots.py
+++ makeEffPlots.py
@@ -43,7 +43,9 @@ class PlotSet:
         grEff.BayesDivide(hwPass, hwTot)
         grEff.SetMarkerStyle(8)
         self.effplots[plotName] = grEff
-       
+    
+
+
 
     #def setLabels(self,plotName,xlabel, ylabel):
        # self.effplots[self.name+'_eff_'+plotName].GetXaxis().SetTitle(xlabel)
@@ -64,7 +66,10 @@ class PlotSet:
         if not isPass:
             return self.plots[name][1]
 
+    def getEffPlot (self, name): 
+        return self.effplots[name]
 
+    
 Plots = PlotSet("Plots")
 
 
@@ -73,14 +78,31 @@ Plots = PlotSet("Plots")
 # opt22Plots.addPlot ('nVtx', 35, 5   , 40)
 # opt21Plots.addPlot ('nVtxNoIso', 35, 5  , 40)
 
-Plots.addPlot('Ebjet1',15,0,300)
-Plots.addPlot('Ebjet2',15,0,300)
-Plots.addPlot('Ebjet',15,0,300)
+Plots.addPlot('Et_bjet1',15,0,300)
+Plots.addPlot('Et_bjet2',15,0,300)
+Plots.addPlot('Et_bjet',15,0,300)
+
+Plots.addPlot('Et_bjet_cut1',15,0,300)
+Plots.addPlot('Et_bjet_cut2',15,0,300)
+Plots.addPlot('Et_bjet_cut3',15,0,300)
+Plots.addPlot('Et_bjet_cut4',15,0,300)
+
+
+Plots.addPlot('Pt_bjet1',15,0,300)
+Plots.addPlot('Pt_bjet2',15,0,300)
+Plots.addPlot('Pt_bjet',15,0,300)
+
+
 Plots.addPlot('PtTau1',15,0,200)
 Plots.addPlot('PtTau2',15,0,200)
 Plots.addPlot('PtTau',15,0,200)
 
-# nVtxBinning = [0, 5, 7, 9, 10, 11, 12, 13 , 14 , 15 , 16 , 17 , 18 , 19 , 20 , 21 , 22 , 23 , 24 , 25 , 28, 30, 40]
+Plots.addPlot('MergedJets_dR',15,0,5)
+Plots.addPlot('MergedJets_Hpt',15,0,500)
+
+
+
+# Nvtxbinning = [0 , 5, 7, 9, 10, 11, 12, 13 , 14 , 15 , 16 , 17 , 18 , 19 , 20 , 21 , 22 , 23 , 24 , 25 , 28, 30, 40]
 # nVtxBinning = [0, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 28, 30, 40, 50, 60]
 #nVtxBinning = [5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 28, 30]
 # nVtxBinning = [0, 15, 50]
@@ -92,12 +114,12 @@ Plots.addPlot('PtTau',15,0,200)
 #opt21Plots.addPlotUserBinning ('nVtxAntiIso', nVtxBinning)
 #opt22Plots.addPlotUserBinning ('nVtxAntiIso', nVtxBinning)
 
-#Plots.setLabels('Ebjet1','E_{bjet1}','Efficiency')
-#Plots.setLabels('Ebjet2','E_{bjet2}','Efficiency')
+#Plots.setLabels('Et_bjet1','E_{bjet1}','Efficiency')
+#Plots.setLabels('Et_bjet2','E_{bjet2}','Efficiency')
 
 
 #### 
-fIn   = TFile.Open('skim_stage2_6jets_sorting3/output.root')
+fIn   = TFile.Open('skim_stage2_jetstau/output.root')
 tIn   = fIn.Get('HTauTauTree')
 nEvt  = tIn.GetEntries()
 optXXplots = Plots
@@ -106,22 +128,87 @@ for ev in range(0, nEvt):
 
 
     
-    #isOS = tIn.isOS
+    
     passes1      = tIn.DeltaRmin_stage2jet_bjet1 < 0.5
-    Ebjet1 = tIn.bjet1_et
     passes2      = tIn.DeltaRmin_stage2jet_bjet2 < 0.5
-    Ebjet2 = tIn.bjet2_et
+    Et_bjet1 = tIn.bjet1_et   
+    Et_bjet2 = tIn.bjet2_et
+    Pt_bjet1 = tIn.bjet1_pt   
+    Pt_bjet2 = tIn.bjet2_pt
+    isJetMerged =  tIn.Bjet2matchesStage2jet1==1
+    dib_deltaR =  tIn.dib_deltaR
+    bH_pt = tIn.bH_pt
 
+    cut1_jet1 = tIn.stage2_jet1Et>36
+    cut2_jet1 = tIn.stage2_jet1Et>68
+    cut3_jet1 = tIn.stage2_jet1Et>128
+    cut4_jet1 = tIn.stage2_jet1Et>200
 
-    optXXplots.getPlot('Ebjet1',False).Fill(Ebjet1)
-    if (passes1): optXXplots.getPlot('Ebjet1',True).Fill(Ebjet1)
-    optXXplots.getPlot('Ebjet2',False).Fill(Ebjet2)
-    if (passes2): optXXplots.getPlot('Ebjet2',True).Fill(Ebjet2)
+    cut1_jet2 = tIn.stage2_jet2Et>36
+    cut2_jet2 = tIn.stage2_jet2Et>68
+    cut3_jet2 = tIn.stage2_jet2Et>128
+    cut4_jet2 = tIn.stage2_jet2Et>200
+
+    
+    optXXplots.getPlot('Et_bjet1',False).Fill(Et_bjet1)
+    if (passes1): optXXplots.getPlot('Et_bjet1',True).Fill(Et_bjet1)
+    optXXplots.getPlot('Et_bjet2',False).Fill(Et_bjet2)
+    if (passes2): optXXplots.getPlot('Et_bjet2',True).Fill(Et_bjet2)
+    
+    optXXplots.getPlot('Et_bjet',False).Fill(Et_bjet1)
+    optXXplots.getPlot('Et_bjet',False).Fill(Et_bjet2)
+    if (passes1): optXXplots.getPlot('Et_bjet',True).Fill(Et_bjet1)
+    if (passes2): optXXplots.getPlot('Et_bjet',True).Fill(Et_bjet2)
+
+    
+
+    optXXplots.getPlot('Pt_bjet1',False).Fill(Pt_bjet1)
+    if (passes1): optXXplots.getPlot('Pt_bjet1',True).Fill(Pt_bjet1)
+    optXXplots.getPlot('Pt_bjet2',False).Fill(Pt_bjet2)
+    if (passes2): optXXplots.getPlot('Pt_bjet2',True).Fill(Pt_bjet2)
+    
+    optXXplots.getPlot('Pt_bjet',False).Fill(Pt_bjet1)
+    optXXplots.getPlot('Pt_bjet',False).Fill(Pt_bjet2)
+    if (passes1): optXXplots.getPlot('Pt_bjet',True).Fill(Pt_bjet1)
+    if (passes2): optXXplots.getPlot('Pt_bjet',True).Fill(Pt_bjet2)
+
+    
    
-    optXXplots.getPlot('Ebjet',False).Fill(Ebjet1)
-    optXXplots.getPlot('Ebjet',False).Fill(Ebjet2)
-    if (passes1): optXXplots.getPlot('Ebjet',True).Fill(Ebjet1)
-    if (passes2): optXXplots.getPlot('Ebjet',True).Fill(Ebjet2)
+    optXXplots.getPlot('Et_bjet_cut1',False).Fill(Et_bjet1)
+    optXXplots.getPlot('Et_bjet_cut1',False).Fill(Et_bjet2)
+    if (passes1 and cut1_jet1): optXXplots.getPlot('Et_bjet_cut1',True).Fill(Et_bjet1)
+    if (passes2 and isJetMerged and cut1_jet1): optXXplots.getPlot('Et_bjet_cut1',True).Fill(Et_bjet2)
+    if (passes2 and cut1_jet2) and not (isJetMerged): optXXplots.getPlot('Et_bjet_cut1',True).Fill(Et_bjet2)
+    
+    optXXplots.getPlot('Et_bjet_cut2',False).Fill(Et_bjet1)
+    optXXplots.getPlot('Et_bjet_cut2',False).Fill(Et_bjet2)
+    if (passes1 and cut2_jet1): optXXplots.getPlot('Et_bjet_cut2',True).Fill(Et_bjet1)
+    if (passes2 and isJetMerged and cut2_jet1): optXXplots.getPlot('Et_bjet_cut2',True).Fill(Et_bjet2)
+    if (passes2 and cut2_jet2) and not (isJetMerged): optXXplots.getPlot('Et_bjet_cut2',True).Fill(Et_bjet2)
+
+    optXXplots.getPlot('Et_bjet_cut3',False).Fill(Et_bjet1)
+    optXXplots.getPlot('Et_bjet_cut3',False).Fill(Et_bjet2)
+    if (passes1 and cut3_jet1): optXXplots.getPlot('Et_bjet_cut3',True).Fill(Et_bjet1)
+    if (passes2 and isJetMerged and cut3_jet1): optXXplots.getPlot('Et_bjet_cut3',True).Fill(Et_bjet2)
+    if (passes2 and cut3_jet2) and not (isJetMerged): optXXplots.getPlot('Et_bjet_cut3',True).Fill(Et_bjet2)
+
+
+    optXXplots.getPlot('Et_bjet_cut4',False).Fill(Et_bjet1)
+    optXXplots.getPlot('Et_bjet_cut4',False).Fill(Et_bjet2)
+    if (passes1 and cut4_jet1): optXXplots.getPlot('Et_bjet_cut4',True).Fill(Et_bjet1)
+    if (passes2 and isJetMerged and cut4_jet1): optXXplots.getPlot('Et_bjet_cut4',True).Fill(Et_bjet2)
+    if (passes2 and cut4_jet2) and not (isJetMerged): optXXplots.getPlot('Et_bjet_cut4',True).Fill(Et_bjet2)
+
+    
+    
+
+
+    optXXplots.getPlot('MergedJets_dR',False).Fill(dib_deltaR)
+    if (isJetMerged): optXXplots.getPlot('MergedJets_dR',True).Fill(dib_deltaR)
+
+    optXXplots.getPlot('MergedJets_Hpt',False).Fill(bH_pt)
+    if (isJetMerged): optXXplots.getPlot('MergedJets_Hpt',True).Fill(bH_pt)
+
 
     passes1tau      = tIn.DeltaRmin_stage2tau_tau1 < 0.1
     PtTau1 = tIn.dau1_pt
@@ -132,7 +219,9 @@ for ev in range(0, nEvt):
     ISO1 =tIn.dau1_MVAiso >=3
     ISO2 =tIn.dau2_MVAiso >=3
     PassIsOS =tIn.isOS==1   
-    
+    isTauMerged =  tIn.Lepton2matchesStage2tau1==1
+    ditau_deltaR =  tIn.ditau_deltaR
+    tauH_pt = tIn.tauH_pt
 
     if (isTau1 and PassIsOS): optXXplots.getPlot('PtTau1',False).Fill(PtTau1)
     if (passes1tau and isTau1 and PassIsOS): optXXplots.getPlot('PtTau1',True).Fill(PtTau1)
@@ -144,16 +233,51 @@ for ev in range(0, nEvt):
     if (passes1tau and isTau1 and PassIsOS): optXXplots.getPlot('PtTau',True).Fill(PtTau1)
     if (passes2tau and isTau2 and PassIsOS): optXXplots.getPlot('PtTau',True).Fill(PtTau2)
    
+
+
+
+
 fIn.Close()
 
 
 
-Plots.makeEffPlot('Ebjet1')
-Plots.makeEffPlot('Ebjet2')
-Plots.makeEffPlot('Ebjet')
+Plots.makeEffPlot('Et_bjet1')
+Plots.makeEffPlot('Et_bjet2')
+Plots.makeEffPlot('Et_bjet')
+Plots.makeEffPlot('Et_bjet_cut1')
+Plots.makeEffPlot('Et_bjet_cut2')
+Plots.makeEffPlot('Et_bjet_cut3')
+Plots.makeEffPlot('Et_bjet_cut4')
+
+Plots.makeEffPlot('Pt_bjet1')
+Plots.makeEffPlot('Pt_bjet2')
+Plots.makeEffPlot('Pt_bjet')
+
+
+Plots.makeEffPlot('MergedJets_dR')
+Plots.makeEffPlot('MergedJets_Hpt')
 Plots.makeEffPlot('PtTau1')
 Plots.makeEffPlot('PtTau2')
 Plots.makeEffPlot('PtTau')
+#Plots.makeEffPlot('MergedTaus_dR')
+#Plots.makeEffPlot('MergedTaus_Hpt')
 
-fOut = TFile ("skim_stage2_6jets_sorting3/effPlots.root", "recreate")
+
+fOut= TFile ("skim_stage2_jetstau/effPlots.root", "recreate")
 Plots.saveToFile(fOut)
+
+multiGraph=TMultiGraph()   
+Plots.getEffPlot('Et_bjet_cut1').SetMarkerColor(4)
+Plots.getEffPlot('Et_bjet_cut2').SetMarkerColor(3)
+Plots.getEffPlot('Et_bjet_cut3').SetMarkerColor(5)
+Plots.getEffPlot('Et_bjet_cut4').SetMarkerColor(2)
+multiGraph.Add(Plots.getEffPlot('Et_bjet'))
+multiGraph.Add(Plots.getEffPlot('Et_bjet_cut1'))
+multiGraph.Add(Plots.getEffPlot('Et_bjet_cut2'))
+multiGraph.Add(Plots.getEffPlot('Et_bjet_cut3'))
+multiGraph.Add(Plots.getEffPlot('Et_bjet_cut4'))
+fOut.cd()
+multiGraph.Write()
+
+
+
