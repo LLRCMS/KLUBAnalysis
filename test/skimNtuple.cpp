6488b396b8f4bc9ddb4a213e6ebd86b2caddba16
Francesco Brivio <francesco.brivio@cern.ch>
2018-03-20 13:18:27 +0100

    fix bug in skimNtuple.cpp

--- test/skimNtuple.cpp
+++ test/skimNtuple.cpp
@@ -109,7 +109,7 @@ void appendFromFileList (TChain* chain, TString filename)
       while (line.find(" ") != std::string::npos) line = line.erase(line.find(" "), 1); // remove white spaces
       while (line.find("\n") != std::string::npos) line = line.erase(line.find("\n"), 1); // remove new line characters
       while (line.find("\r") != std::string::npos) line = line.erase(line.find("\r"), 1); // remove carriage return characters
-vai      if (!line.empty()) // skip empty lines
+      if (!line.empty()) // skip empty lines
 	chain->Add(line.c_str());
     }
   return;
