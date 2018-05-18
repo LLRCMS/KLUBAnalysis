e658ecf04a98daf22f4d2b82afc229b824691faa
Francesco Brivio <francesco.brivio@cern.ch>
2018-05-18 17:25:19 +0200

    more work on PU reweight

--- weights/PUreweight/makeRatio.C
+++ weights/PUreweight/makeRatio.C
@@ -6,16 +6,21 @@ void makeRatio ()
 
     cout << "MC: " << hPUMC->GetName() << endl;
 
-    TFile * fileData = TFile::Open("MyDataPileupHistogram_2016data.root");
-    TH1D * hPUData = (TH1D*)fileData->Get("pileup");
-
-    cout << "DATA: " << hPUData->GetName() << endl;
+    //histo DATA
+    //TFile * fileData = TFile::Open("MyDataPileupHistogram_2016data.root");
+    //TH1D * hPUData = (TH1D*)fileData->Get("pileup");
     // //histo Data26
     // TFile * file26 = TFile::Open("MyDataPileupHistogram_92fb.root");
     // TH1F * hPU26 = (TH1F*)file26->Get("pileup");
     // //histo Data40
     // TFile * file40 = TFile::Open("MyDataPileupHistogram_129fb.root");
     // TH1F * hPU40 = (TH1F*)file40->Get("pileup");
+    TFile * fileData = TFile::Open("MyDataPileupHistogram_2017Bdata.root");
+    TH1D * hPUData = (TH1D*)fileData->Get("pileup");
+    //TFile * fileData = TFile::Open("MyDataPileupHistogram_2017Fdata.root");
+    //TH1D * hPUData = (TH1D*)fileData->Get("pileup");
+    
+    cout << "DATA: " << hPUData->GetName() << endl;
 
     //histo ratio
     TH1D * hRatio = new TH1D("hRatio","hRatio",100,0,100);
@@ -34,7 +39,9 @@ void makeRatio ()
     // hRatioScale->Draw();
 
     for (int i = 0 ; i < 100 ; ++i)
-        cout << "npuSummer16_36fb[" << i << "] = " <<hRatio->GetBinContent(i+1)/hRatio->Integral() <<  ";" << endl;
+        //cout << "npuSummer16_36fb[" << i << "] = " <<hRatio->GetBinContent(i+1)/hRatio->Integral() <<  ";" << endl;
+        cout << "npuFall17_4c7fb[" << i << "] = " <<hRatio->GetBinContent(i+1)/hRatio->Integral() <<  ";" << endl; // Run 2017B
+        //cout << "npuFall17_13c4fb[" << i << "] = " <<hRatio->GetBinContent(i+1)/hRatio->Integral() <<  ";" << endl; // Run 2017F
 
     hPUMC->SetLineColor(kBlack);
     hPUData->SetLineColor(kRed);
