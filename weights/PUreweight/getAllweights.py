0b7cfd6747b86a4041ace998c62da1ea46b6ca2d
camendola <chia.amendola@gmail.com>
2018-08-02 11:14:10 +0200

    fix to PU script for txt files

--- weights/PUreweight/getAllweights.py
+++ weights/PUreweight/getAllweights.py
@@ -11,12 +11,20 @@ for i, row in fileNames.iterrows():
     print row["name"], row["filelist"]
     filepath = "../../inputFiles/Files_June2018/"+row["filelist"]
     name  = row["name"]
-    command = "root -l -b \"ratioPU.C(0, 50000000, \\\""+filepath+"\\\", \\\""+name+"\\\")\" -q ; root -l \"makeRatio.C(\\\""+name+"\\\")\" > outputs/"+name+".cc -q"
+    command = "root -l -b \"ratioPU.C(0, 50000000, \\\""+filepath+"\\\", \\\""+name+"\\\")\" -q ; root -l \"makeRatio.C(\\\""+name+"\\\")\" > outputs/"+name+".txt -q"
 
     print command
     os.system(command)
     os.system("rm "+name+".root")
-    with  open("outputs/"+name+".cc", 'r') as fin:
+
+    with  open("outputs/"+name+".txt", 'r') as fin:
         data = fin.read().splitlines(True)
-    with open("outputs/"+name+".cc", 'w') as fout:
+        for k in range(len(data)):
+            weight= []
+            if k < 4: continue;
+            weight= data[k].split("=")
+            data[k] = weight[1].replace(";","")
+            
+
+    with open("outputs/"+name+".txt", 'w') as fout:
         fout.writelines(data[4:])
