f4f1e52956fa7d65a7c0c8a9d272520781f44d52
Francesco Brivio <francesco.brivio@cern.ch>
2018-08-10 22:26:46 +0200

    last modifications 10 August 2018

--- scripts/makePlotsFinal.sh
+++ scripts/makePlotsFinal.sh
@@ -6,7 +6,14 @@
 #tag=14Jul2018_style
 #tag=19Jul2018
 #tag=20Jul2018
-tag=25Jul2018_reduced_oldDY
+#tag=25Jul2018_reduced_PU
+#tag=25Jul2018_newPU
+#tag=25Jul2018_newPU_noSF
+#tag=27Jul2018_pt45
+#tag=27Jul2018_pt45_noSF
+#tag=28Jul2018_newSF
+#tag=30Jul2018_newPU_oldSF
+tag=8Aug2018
 
 log=(--log)
 
@@ -16,12 +23,12 @@ channel=TauTau
 
 #lumi=13.4
 lumi=41.6
-reg=SR  # A:SR , B:SStight , C:OSinviso, D:SSinviso
+reg=SR  # A:SR , B:SStight , C:OSinviso, D:SSinviso, B': SSrlx
 
 #baseline=baseline
 ##baseline=baselineMassCut
 
-baseline=s1b1jresolved
+#baseline=s1b1jresolved
 #baseline=s1b1jresolvedMcut
 
 #baseline=s2b0jresolved
@@ -30,7 +37,7 @@ baseline=s1b1jresolved
 #baseline=sboostedLL
 #baseline=sboostedLLMcut
 
-#baseline=antiB
+baseline=antiB
 
 #baseline=baselineVBF
 #baseline=baselineNoVBF
@@ -60,7 +67,7 @@ do
     #python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var bjet1_eta  --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag --label "#eta_{bjet1}" --no-sig $others --quit
     #python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var bjet2_eta  --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag --label "#eta_{bjet2}" --no-sig $others --quit
 
-    python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var bH_mass        --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi  ${log[i]} --ratio  --tag $tag --label "H_{bb} mass [GeV]" --no-sig $others --quit
+    python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var bH_mass        --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi  ${log[i]} --ratio  --tag $tag --label "m_{bb} [GeV]" --no-sig $others --quit
     ##python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var dib_dEtaSign   --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi  ${log[i]} --ratio  --tag $tag --label "#eta_{bjet1}x#eta_{bjet2}" --no-sig $others --quit
     #python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var dib_deltaR     --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi  ${log[i]} --ratio  --tag $tag --label "#DeltaR_{bb}" --no-sig $others --quit
 
@@ -69,13 +76,13 @@ do
   #
   ##taus
     python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var dau1_pt         --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag --label "p_{T#tau1} [GeV]" --no-sig $others --quit
-    ##python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var dau2_pt         --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag --label "p_{T#tau2} [GeV]" --no-sig $others --quit
+    python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var dau2_pt         --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag --label "p_{T#tau2} [GeV]" --no-sig $others --quit
 
-    #python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var dau1_eta        --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag --label "#eta_{#tau1}" --no-sig $others --quit
-    #python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var dau2_eta        --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag --label "#eta_{#tau2}" --no-sig $others --quit
+    ##python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var dau1_eta        --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag --label "#eta_{#tau1}" --no-sig $others --quit
+    ##python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var dau2_eta        --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag --label "#eta_{#tau2}" --no-sig $others --quit
 
-    #python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var dau1_phi        --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag --label "#phi_{#tau1}" --no-sig $others --quit
-    #python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var dau2_phi        --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag --label "#phi_{#tau2}" --no-sig $others --quit
+    ##python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var dau1_phi        --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag --label "#phi_{#tau1}" --no-sig $others --quit
+    ##python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var dau2_phi        --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag --label "#phi_{#tau2}" --no-sig $others --quit
 
 
     #python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var dau1_iso        --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag --label "Iso_{#tau1} [GeV]" --no-sig $others --quit
@@ -84,8 +91,8 @@ do
     #python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var dau2_MVAisoNew  --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag --label "MVAisoNew_{#tau2} [GeV]" --no-sig $others --quit
 
 
-    python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var tauH_SVFIT_mass --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag --label "H_{#tau#tau}^{SVfit} [GeV]" --no-sig $others --quit
-    python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var tauH_mass       --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag --label "H_{#tau#tau}^{vis} [GeV]" --no-sig $others --quit
+    ##python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var tauH_SVFIT_mass --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag --label "m_{#tau#tau}^{SVfit} [GeV]" --no-sig $others --quit
+    python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var tauH_mass       --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag --label "m_{#tau#tau}^{vis} [GeV]" --no-sig $others --quit
 
     #python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var tauH_pt         --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag --label "p_{TH#tau#tau}^{Vis} [GeV]" --no-sig $others --quit
     #python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var ditau_deltaEta         --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} -#-ratio  --tag $tag --label "#Delta#eta_{#tau#tau}" --no-sig $others --quit
@@ -104,7 +111,7 @@ do
     ##python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var VBFjj_dEtaSign --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag  --label "#eta_{VBFj11}#times#eta_{VBFj2}" --no-sig $others --quit
     ##python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var VBFjj_deltaEta --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag  --label "#Delta#eta_{jj}" --no-sig $others --quit
 
-    ##python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var VBFjj_mass  --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag  --label "M_{jj}^{VBF} [GeV]" --no-sig $others --quit
+    ##python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var VBFjj_mass  --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag  --label "m_{jj}^{VBF} [GeV]" --no-sig $others --quit
 
     ##python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var VBFjet1_pt  --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag  --label "p_{TVBFjet1} [GeV]" --no-sig $others --quit
     ##python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var VBFjet2_pt  --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag  --label "p_{TVBFjet2} [GeV]" --no-sig $others --quit
@@ -129,8 +136,8 @@ do
   #
   ##python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var BDT_MET_bH_cosTheta    --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag --label "cos#theta(H_{bb}, MET)" --no-sig $others --quit
   ##python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var BDT_HT20    --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag --label "HT [GeV]" --no-sig $others --quit
-  ##python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var HHKin_mass_raw    --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag --label "m_{HH}^{KinFit} [GeV]" --no-sig $others --quit
-  ##python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var MT2    --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag --label "MT2 [GeV]" --no-sig $others --quit
+  ##python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var HHKin_mass_raw    --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag --label "m_{HH}^{KinFit} [GeV]" --no-sig $others --blind-range 250 1000 --quit
+  ##python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var MT2    --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag --label "MT2 [GeV]" --no-sig $others --blind-range 200 500 --quit
   ##python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var bH_pt    --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag --label "p_{TH_{bb}} [GeV]" --no-sig $others --quit
   ##python scripts/makeFinalPlots.py --dir analysis_$channel\_$tag --var p_zeta    --reg $reg --sel $baseline --channel $channel --lymin 0.7 --lumi $lumi ${log[i]} --ratio  --tag $tag --label "P_{#zeta}" --no-sig $others --quit
 
