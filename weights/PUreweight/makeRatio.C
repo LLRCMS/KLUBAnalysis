2108e93f6f71a3e3822827110d50c0ed2019f4bc
Francesco Brivio <francesco.brivio@cern.ch>
2020-06-02 12:33:58 +0200

    fix 2016 PUreweight

--- weights/PUreweight/makeRatio.C
+++ weights/PUreweight/makeRatio.C
@@ -1,5 +1,5 @@
-//void makeRatio (string const &myFileName = "MyMCPileupHistogram_TT2016")
-void makeRatio (string const &myFileName = "MyMCPileupHistogram_TT2017_24Apr2020")
+void makeRatio (string const &myFileName = "MyMCPileupHistogram_TT2016")
+//void makeRatio (string const &myFileName = "MyMCPileupHistogram_TT2017")
 //void makeRatio (string const &myFileName = "MyMCPileupHistogram_TT2018")
 {
   gROOT->SetBatch();
@@ -8,8 +8,8 @@ void makeRatio (string const &myFileName = "MyMCPileupHistogram_TT2017_24Apr2020
     TH1D * hPUMC   = (TH1D*)fileMC->Get("myPUHisto");
 
     //histo DATA
-    //TFile * fileData = TFile::Open("MyDataPileupHistogram_Legacy2016.root");
-    TFile * fileData = TFile::Open("MyDataPileupHistogram_Legacy2017.root");
+    TFile * fileData = TFile::Open("MyDataPileupHistogram_Legacy2016.root");
+    //TFile * fileData = TFile::Open("MyDataPileupHistogram_Legacy2017.root");
     //TFile * fileData = TFile::Open("MyDataPileupHistogram_Legacy2018.root");
     TH1D * hPUData = (TH1D*)fileData->Get("pileup");
 
