082abfa7526c96ab15b3f2bfa178e1b3f1230743
Francesco Brivio <francesco.brivio@cern.ch>
2020-09-07 10:51:49 +0200

    add scripts for VBF limits

---
+++ limits/VBFlimits/prepare_histos.sh
@@ -0,0 +1,7 @@
+# Two steps for this:
+
+# 1 - prepare files with yield variations --> REMEMBER to modify hardcoded strings (channel, folder...)
+#python compute_scales.py -y 2018
+
+# 2 - prepare histograms with systematics
+#python prepare_histos.py -f ../../analysis_TauTau_synch_11June2020_limits_prova/analyzedOutPlotter.root -o prova -c TauTau -y 2018
