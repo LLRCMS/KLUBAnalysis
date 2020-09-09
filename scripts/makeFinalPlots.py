843db395b5a0131c8a7f8c27b9565cd5e5b92af7
Francesco Brivio <francesco.brivio@cern.ch>
2020-09-09 22:31:12 +0200

    add VBF categories in plotting scripts

--- scripts/makeFinalPlots.py
+++ scripts/makeFinalPlots.py
@@ -855,6 +855,20 @@ if __name__ == "__main__" :
                     selName = "1b1j"
             if "2b0j" in args.sel:  
                     selName = "2b0j"
+            if "VBFloose" in args.sel:
+                    selName = "VBF loose"
+            if "GGFclass" in args.sel:
+                    selName = "VBF - GGF mpp"
+            if "VBFclass" in args.sel:
+                    selName = "VBF - VBF mpp"
+            if "TTlepclass" in args.sel:
+                    selName = "VBF - TTlep mpp"
+            if "TThadclass" in args.sel:
+                    selName = "VBF - TThad mpp"
+            if "ttHclass" in args.sel:
+                    selName = "VBF - ttH mpp"
+            if "DYclass" in args.sel:
+                    selName = "VBF - DY mpp"
     else:
             selName = args.name
 
