77fc9890f3ad1b006a7ef12161ec9019846410a5
Jona Motta <jona.motta@polytechnique.edu>
2021-01-04 14:38:13 +0100

    update of ttSF scripts and configs to apply btagReshape

--- ttSF/CRlimits.sh
+++ ttSF/CRlimits.sh
@@ -2,7 +2,7 @@
 # and gives as output the comb.root file containing teh fitted ttSF from a CR only fit
 # 
 # If you have used the ttCR_inclusiveOutPlotter_creator.sh sccript on CR first, then you need 
-# to comment out everything up to the call of write_cards_ttCRstudy.py (not included)
+# to comment out everything up to the call of write_cards_ttCR.py (not included)
 #
 # To make this work we first have to:
 # 	1. crate the following folders: 
@@ -16,30 +16,28 @@
 # This will create the .root files that then have to be passed to the write_cards script 
 
 
-export Y="2017"
+export Y="2018"
 export CHANNELS="TauTau MuTau ETau"
 
 
 for ch in $CHANNELS
 do 
 	cd ../ttCRlimits_${Y}inclusive/${ch}/
-	hadd outPlotter${ch}.root outPlotter_*.root 
+	hadd outPlotter.root outPlotter_*.root
 	rm outPlotter_*.root
+	python ../../scripts/combineFillerOutputs.py --dir ../../ttCRlimits_${Y}inclusive/${ch}/
 	cd -
 done 
 
 cd ../ttCRlimits_${Y}inclusive/
-hadd outPlotter.root ETau/outPlotterETau.root MuTau/outPlotterMuTau.root TauTau/outPlotterTauTau.root 
+hadd analyzedOutPlotter.root ETau/analyzedOutPlotter.root MuTau/analyzedOutPlotter.root TauTau/analyzedOutPlotter.root 
 cp TauTau/main*.cfg ./mainCfg_ttCR_Legacy${Y}.cfg
 
-cd ..
-python scripts/combineFillerOutputs.py --dir ttCRlimits_${Y}inclusive/
-
-cd ttSF
+cd ../ttSF
 python prepare_histos.py -f ../ttCRlimits_${Y}inclusive/analyzedOutPlotter.root -o CR -c 'inclusive' -y ${Y}
 mv -v analyzedOutPlotter_${Y}_inclusive_CR.root ../ttCRlimits_${Y}inclusive/
 
-python write_cards_ttCRstudy.py --filename ../ttCRlimits_${Y}inclusive/analyzedOutPlotter_${Y}_inclusive_CR.root --dir '' --channel 'inclusive' --year ${Y} --selection ttCR_invMcut --binbybin 1 --shape 0 --theory --config ../ttCRlimits_${Y}inclusive/main*.cfg --dynamQCD 0
+python write_cards_ttCR.py --filename ../ttCRlimits_${Y}inclusive/analyzedOutPlotter_${Y}_inclusive_CR.root --dir '' --channel 'inclusive' --year ${Y} --selection ttCR_invMcut --binbybin 1 --shape 0 --theory --config ../ttCRlimits_${Y}inclusive/main*.cfg --dynamQCD 0
 
 cd cards_inclusive
 combineCards.py ttCR_invMcutDNNoutSM_kl_1/hh_*.txt >> comb.txt
