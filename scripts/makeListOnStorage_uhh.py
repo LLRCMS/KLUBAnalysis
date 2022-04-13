70b6f473d851e142eeed537649fc18531c75fbba
Marcel R <github.riga@icloud.com>
2022-04-13 18:21:43 +0200

    Add input file lists without xrd doors for 2017.

--- scripts/makeListOnStorage_uhh.py
+++ scripts/makeListOnStorage_uhh.py
@@ -9,9 +9,10 @@ thisdir = os.path.dirname(os.path.abspath(__file__))
 
 
 tag = "uhh_2017_v2"
-kind = "backgrounds"
+kind = "data"
 output_dir = os.path.normpath(os.path.join(thisdir, "..", "inputFiles", "UL17_" + kind))
-xrd_door = "root://dcache-cms-xrootd.desy.de:1094/"
+# xrd_door = "root://dcache-cms-xrootd.desy.de:1094/"
+xrd_door = ""
 
 store_path = "/pnfs/desy.de/cms/tier2/store/user/{}/hbt_resonant_run2/HHNtuples".format({
     "backgrounds": "tokramer",
@@ -61,7 +62,7 @@ for sample_dir in glob.glob(os.path.join(store_path, tag, "*", "*")):
     # define the output file
     output_file = os.path.join(output_dir, "{}__{}.txt".format(sample_name, campaign_name))
     # extended datasets should be added to the original one when existing
-    is_ext = re.match(r"_ext\d+-v.+$", campaign_name)
+    is_ext = re.match(r"^(.+)_ext\d+-v.+$", campaign_name)
     if is_ext:
         print("\nfound dataset extension being saved in output file {}".format(output_file))
         print("please merge it with the original file when existting!\n")
@@ -71,4 +72,5 @@ for sample_dir in glob.glob(os.path.join(store_path, tag, "*", "*")):
         os.makedirs(os.path.dirname(output_file))
     with open(output_file, "w") as f:
         f.write("\n".join(file_names) + "\n")
-    print("found {} files for sample {}".format(sample_name, len(file_names)))
+    print("found {} files for sample {}, campaign {}".format(len(file_names), sample_name,
+        campaign_name))
