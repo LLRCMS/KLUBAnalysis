0c96030452b95335577119d6ddfac85bfc148d17
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-07-18 10:29:22 +0200

    rewrite + single multi-job submission

--- scripts/submitHistoFiller.py
+++ scripts/submitHistoFiller.py
@@ -4,48 +4,61 @@ import argparse
 import datetime
 
 parser = argparse.ArgumentParser(description='Command line parser of plotting options')
-parser.add_argument('--cfg',    dest='cfg',   help='name of the main cfg to run', default=None)
-parser.add_argument('--tag',    dest='tag',   help='name of working space (defaults to timestamp)', default=None)
-parser.add_argument('--njobs',  dest='njobs', type=int, help='number of jobs for parallelization',  default=10)
-args = parser.parse_args()
+parser.add_argument('--cfg', dest='cfg', required=True,
+                    help='name of the main cfg to run', default=None)
+parser.add_argument('--tag', dest='tag', required=True, default=None,
+                    help='name of working space (defaults to timestamp)', )
+parser.add_argument('--njobs', dest='njobs', required=False, type=int,
+                    help='number of jobs for parallelization', default=10)
+FLAGS = parser.parse_args()
 
-if not args.cfg:
-    print "** Please provide a cfg name: --cfg <relative path from KLUBanalysis/ or absolute path>"
-    sys.exit()
+#hourdate = datetime.datetime.now().strftime('%Y.%m.%d_%H.%M.%S').replace('.','-')
+outdir = "filljobs_" + FLAGS.tag
+exe = 'testAnalysisHelper.exe'
+scriptname = 'filler.sh'
+logname = 'log_${1}.txt'
 
-# datetime.datetime.now()
-outDir = datetime.datetime.now().strftime('%Y.%m.%d_%H.%M.%S')
-if args.tag:
-    outDir = args.tag
-outDir = "jobs_"+outDir
+write = lambda stream, text: stream.write(text + '\n')
 
-# pathname = os.path.dirname(sys.argv[0])        
-# here     = os.path.abspath(pathname)
-here = os.getcwd()
+def create_dir(d):
+    if not os.path.exists(d):
+        os.makedirs(d)
+create_dir(outdir)
 
-program = 'testAnalysisHelper.exe'
+scriptpath = os.path.join(outdir, scriptname)
+scriptpath_condor = scriptpath.replace('.sh','.condor')
+joblog = os.path.join(outdir, 'joblogs')
+create_dir(joblog)
+joblog = os.path.join(joblog, logname)
+with open(scriptpath, 'w') as s:
+    write(s,'#!/bin/bash')
+    write(s,'export X509_USER_PROXY=~/.t3/proxy.cert')
+    write(s,'source /cvmfs/cms.cern.ch/cmsset_default.sh')
+    write(s,'cd {}'.format(os.getcwd()))
+    write(s,'export SCRAM_ARCH=slc6_amd64_gcc491')
+    write(s,'eval `scram r -sh`')
+    write(s,'source scripts/setup.sh')
+    command = ( exe + ' ' + FLAGS.cfg + ' ${1}' + ' ' +
+                str(FLAGS.njobs) + ' 2>&1 | tee ' + joblog )
+    write(s,command)
+    os.system('chmod u+rwx ' + os.path.join(outdir, scriptname))
 
-proto = 'filler_' ## job .sh fie name
-logproto = 'log_' ## job .sh fie name
-
-os.system('mkdir ' + outDir)
-for nj in range(0, args.njobs):
-    scriptName = proto + str(nj) + '.sh'
-    logName    = logproto + str(nj) + '.txt'
-    scriptFile = open (outDir + '/' + scriptName, 'w')
-    scriptFile.write ('#!/bin/bash\n')
-    scriptFile.write ('export X509_USER_PROXY=~/.t3/proxy.cert\n')
-    scriptFile.write ('source /cvmfs/cms.cern.ch/cmsset_default.sh\n')
-    scriptFile.write ('cd %s\n' % here)
-    scriptFile.write ('export SCRAM_ARCH=slc6_amd64_gcc491\n')
-    scriptFile.write ('eval `scram r -sh`\n')
-    scriptFile.write ('source scripts/setup.sh\n')
-    command = program + ' ' + args.cfg + ' ' + str(nj) + ' ' + str(args.njobs) + ' 2>&1 | tee ' + outDir + '/' + logName
-    scriptFile.write(command)
-    scriptFile.close()
+condlog = os.path.join(outdir, 'outputs')
+create_dir(condlog)
+with open(scriptpath_condor, 'w') as s:
+    write(s,'Universe = vanilla')
+    write(s,'Executable = {}'.format(scriptpath))
+    write(s,'input = /dev/null')
+    write(s,'output = {}/condor_log_$(Process).o'.format(condlog))
+    write(s,'error  = {}/condor_log_$(Process).e'.format(condlog))
+    write(s,'getenv = true')
+    write(s,'T3Queue = short')
+    write(s,'WNTag=el7')
+    write(s,'+SingularityCmd = ""')
+    write(s,'include : /opt/exp_soft/cms/t3_tst/t3queue |')
+    write(s,'Arguments = $(Process) ')
+    write(s,'queue {}'.format(FLAGS.njobs))
     
-    os.system ('chmod u+rwx ' + outDir + '/' + scriptName)
-    launchcommand = ('/opt/exp_soft/cms/t3/t3submit -short \'' + outDir + '/' + scriptName +"\'")
-    print launchcommand
-    os.system (launchcommand)
-    # command = '/opt/exp_soft/cms/t3/t3submit -short ' + outDir + '/' + proto + str (nj) + '.sh'
+launch_command = 'condor_submit {}'.format(scriptpath_condor)
+print('The following command was run: {}'.format(launch_command))
+os.system(launch_command)
