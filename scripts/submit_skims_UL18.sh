16698a710641e66262a4872515f3f576143baaf4
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-07-26 23:04:13 +0200

    make it similar to the submission script for 2017 samples

--- scripts/submit_skims_UL18.sh
+++ scripts/submit_skims_UL18.sh
@@ -1,109 +1,102 @@
-SUBMITSCRIPT="python scripts/skimNtuple.py"
-SKIMDIR="/data_CMS/cms/${USER}/HHresonant_SKIMS"
-mkdir -p "${SKIMDIR}"
+#!/usr/bin/env bash
 
-######################
 ### Argument parsing
-######################
-help_str="prints this help message"
-tag_str="select tag"
-force_str="whether to override a folder with the same tag"
-dataperiod_str="which data period to consider: Legacy18, UL18, ..."
+HELP_STR="prints this help message"
+TAG_STR="select tag"
+FORCE_STR="whether to override a folder with the same tag"
+DATAPERIOD_STR="which data period to consider: Legacy18, UL18, ..."
 function print_usage_submit_skims {
-    usage=" $(basename "$0") [-h] [-t -f -d]
-where:
-    -h / --help        [ ${help_str} ]
-    -f / --force       [ ${force_str} ]
-    -t / --tag         [ ${tag_str} ]
-    -d / --data_period [ ${dataperiod_str} ]
-    Run example: $(basename "$0") -f
+    USAGE=" $(basename "$0") [-H] [-t -f -d]
+
+    -h / --help        [ ${HELP_STR} ]
+    -t / --tag         [ ${TAG_STR} ]
+    -f / --force       [ ${FORCE_STR} ]
+    -d / --data_period [ ${DATAPERIOD_STR} ]
+
+    Run example: $(basename "$0") -t <some_tag> -f
 "
-    printf "${usage}"
+    printf "${USAGE}"
 }
 
 FORCE="0"
 TAG=""
 DATA_PERIOD="UL18"
 while [[ $# -gt 0 ]]; do
-    key="$1"
-
+    key=${1}
     case $key in
 	-h|--help)
-	        print_usage_submit_skims
-		    exit 1
-		        ;;
+	    print_usage_submit_skims
+	    exit 1
+	    ;;
 	-t|--tag)
-	        TAG="$2"
-		    shift # past argument
-		        shift # past value
-			    ;;
+	    TAG=${2}
+	    shift; shift;
+	    ;;
 	-f|--force)
-	        FORCE="1"
-		    shift # past argument
-		        shift # past value
-			    ;;
+	    FORCE="1"
+	    shift; shift;
+	    ;;
 	-d|--data_period)
-	        DATA_PERIOD="$2"
-		    shift # past argument
-		        shift # past value
-			    ;;
+	    DATA_PERIOD=${2}
+	    shift; shift;
+	    ;;
 	*)  # unknown option
-	        echo "Wrong parameter ${1}."
-		    exit 1
-		        ;;
+	    echo "Wrong parameter ${1}."
+	    exit 1
+	    ;;
     esac
 done
 
-# Verify if a tag was assigned by the user
-# If not, print already existing tags (if any) and exits
-if [[ -z "${TAG}" ]]; then
-    echo "Select the tag via the '--tag' option."
-    declare -a tags=( $(/bin/ls -1 "${SKIMDIR}") )
+### Setup variables
+THIS_FILE="$( [ ! -z "$ZSH_VERSION" ] && echo "${(%):-%x}" || echo "${BASH_SOURCE[0]}" )"
+THIS_DIR="$( cd "$( dirname ${THIS_FILE} )" && pwd )"
+KLUB_DIR="$( cd "$( dirname ${THIS_DIR} )" && pwd )"
+
+SUBMIT_SCRIPT="scripts/skimNtuple.py"
+SKIM_DIR="/data_CMS/cms/${USER}/HHresonant_SKIMS"
+mkdir -p ${SKIM_DIR}
+
+IN_DIR="/home/llr/cms/portales/hhbbtautau/KLUB_UL_20220321/CMSSW_11_1_9/src/KLUBAnalysis/inputFiles/"
+SIG_DIR=${IN_DIR}${DATA_PERIOD}"_signals/"
+BKG_DIR=${IN_DIR}${DATA_PERIOD}"_backgrounds/"
+DATA_DIR=${IN_DIR}${DATA_PERIOD}"_data/"
+
+PU_DIR="weights/PUreweight/UL_Run2_PU_SF/2018/PU_UL2018_SF.txt"
+CFG="config/skim_${DATA_PERIOD}.cfg"
+PREF="SKIMS_"
+OUT_DIR=${PREF}${DATA_PERIOD}"_"${TAG}
+OUTSKIM_DIR=${SKIM_DIR}/${OUT_DIR}/
+
+declare -A IN_LIST DATA_MAP
+declare -a DATA_LIST RUNS MASSES
+
+### Argument parsing sanity checks
+if [[ -z ${TAG} ]]; then
+    printf "Select the tag via the '--tag' option. "
+    declare -a tags=( $(/bin/ls -1 ${SKIM_DIR}) )
     if [ ${#tags[@]} -ne 0 ]; then
-	echo "The following tags are currently available (the prefix is not part of the tag):"
-	for tag in "${tags[@]}"; do
-	    echo "- ${tag}"
-	    done
+		echo "The following tags are currently available:"
+		for tag in ${tags[@]}; do
+			echo "- ${tag/${PREF}${DATA_PERIOD}_/}" # bash string substitution
+		done
     else
-	echo "No tags are currently available. Everything looks clean!"
+		echo "No tags are currently available. Everything looks clean!"
     fi
     exit 1;
 fi
-
-# Verify if a data period is defined
 if [[ -z ${DATA_PERIOD} ]]; then
-    echo "Select the data period via the '--d / --data_period' option."
-    exit 1;
+	echo "Select the data period via the '--d / --data_period' option."
+	exit 1;
 fi
 
-#####################
-### Setup variables
-#####################
-DATA_INPUT="/home/llr/cms/portales/hhbbtautau/KLUB_UL_20220321/CMSSW_11_1_9/src/KLUBAnalysis/inputFiles/"
-SIGDIR=${DATA_INPUT}${DATA_PERIOD}"_signals/"
-BKGDIR=${DATA_INPUT}${DATA_PERIOD}"_backgrounds/"
-DATADIR=${DATA_INPUT}${DATA_PERIOD}"_data/"
+#### Source additional setup
+source scripts/setup.sh
+source /opt/exp_soft/cms/t3/t3setup
+mkdir -p ${OUTSKIM_DIR}
+cp scripts/listAll.sh ${OUTSKIM_DIR}
 
-PUSF="weights/PUreweight/UL_Run2_PU_SF/2018/PU_UL2018_SF.txt"
-THECFG="config/skim_${DATA_PERIOD}.cfg"
-OUTDIR="SKIMS_"${DATA_PERIOD}"_"${TAG}
-OUTSKIMDIR="${SKIMDIR}/${OUTDIR}/"
-PREF="SKIM_"
-
-declare -A InputList datasetsAssoc;
-declare -a datasets runs masses;
-
-# source scripts/setup.sh
-# source /opt/exp_soft/cms/t3/t3setup
-mkdir -p ${OUTSKIMDIR}
-# touch ${OUTSKIMDIR}"README.txt"
-# echo "KLUB UL skimming" > $SKIMDIR/$OUTDIR/README.txt
-cp scripts/listAll.sh ${OUTSKIMDIR}
-
-######################
 ### Input files list
-######################
-InputList=(
+IN_LIST=(
 	# Data
 	["EGamma_Run2018A"]="1_EGamma__Run2018A-UL2018_MiniAODv2-v1.txt"
 	["EGamma_Run2018B"]="2_EGamma__Run2018B-UL2018_MiniAODv2-v1.txt"
@@ -298,106 +291,102 @@ InputList=(
 	["TTWZ"]="47_TTWZ_TuneCP5_13TeV-madgraph-pythia8__RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v1.txt"
 )
 
-###########################
-### Submission commands ###
-###########################
-function BASE_COMMAND_SIGNAL() {
-    local retval="${SUBMITSCRIPT} -T ${OUTDIR} -d True -s True -c ${THECFG} -q long -Y 2018 -k True -f ${FORCE}"
-    echo ${retval}
-}
-function BASE_COMMAND() {
-    local retval="$(BASE_COMMAND_SIGNAL) --pu ${PUSF}"
-    echo ${retval}
+### Submission command
+function run_skim() {
+	python ${KLUB_DIR}/${SUBMIT_SCRIPT} -T ${OUT_DIR} -s True -c ${KLUB_DIR}/${CFG} -q long -Y 2018 -k True --pu ${PU_DIR} -f ${FORCE} "$@"
 }
 
-# #### DATA - filelists up to date
-# datasets=("EGamma" "Tau" "SingleMuon" "MET");
-# runs=("Run2018A" "Run2018B" "Run2018C" "Run2018D");
-# for ds in "${datasets[@]}"; do
-# 	for run in "${runs[@]}"; do
-# 		file="${ds}_${run}";
-# 		$(BASE_COMMAND_SIGNAL) -n 10 -o ${OUTSKIMDIR}${PREF}${file} -i ${DATADIR}${InputList[${file}]}
-# 	done
-# done
+### Data
+DATA_LIST=("EGamma" "Tau" "SingleMuon" "MET")
+RUNS=("Run2018A" "Run2018B" "Run2018C" "Run2018D")
+for ds in ${DATA_LIST[@]}; do
+	for run in ${RUNS[@]}; do
+		file="${ds}_${run}"
+		run_skim -n 10 -d True -o ${OUTSKIM_DIR}${PREF}${file} -i ${DATA_DIR}${IN_LIST[${file}]}
+	done
+done
  
-# #### HH resonant signal - all masspoints
-# # spin-0 ggF
-# datasets=("ggF_Radion" "ggF_BulkGraviton" "VBF_Radion" "VBF_BulkGraviton");
-# masses=("250" "260" "270" "280" "300" "320" "350" "400" "450" "500" "550" "600" "650" "700" "750" "800" "850" "900" "1000" "1250" "1500" "1750" "2000" "2500" "3000");
-# for ds in "${datasets[@]}"; do
-# for mass in "${masses[@]}"; do
-# file="${ds}_m${mass}";
-# $(BASE_COMMAND) -n 20 -o ${OUTSKIMDIR}${PREF}${file} -i ${SIGDIR}${InputList[${file}]} -x 1.
-# done
-# done
+### HH resonant signal
+DATA_LIST=("ggF_Radion" "ggF_BulkGraviton" "VBF_Radion" "VBF_BulkGraviton")
+MASSES=("250" "260" "270" "280" "300" "320" "350" "400" "450" "500" "550" "600" "650" "700" "750" "800" "850" "900" "1000" "1250" "1500" "1750" "2000" "2500" "3000")
+for ds in ${DATA_LIST[@]}; do
+	for mass in ${MASSES[@]}; do
+		file="${ds}_m${mass}";
+		run_skim -n 20 -o ${OUTSKIM_DIR}${PREF}${file} -i ${SIG_DIR}${IN_LIST[${file}]} -x 1.
+	done
+done
 
-# ##### TT - XS Taken from HTT http://cms.cern.ch/iCMS/user/noteinfo?cmsnoteid=CMS%20AN-2019/109
-# ### TT x section: 831.76 for inclusive sample, W->had 67,60% , W->l nu 3*10,8% = 32,4% (sum over all leptons)
-# ### hh = 45.7%, ll = 10.5%, hl = 21.9% (x2 for permutation t-tbar)
-# datasetsAssoc=( ["TT_fullyHad"]="377.96"
-# 				["TT_fullyLep"]="88.29"
-# 				["TT_semiLep"]="365.34"
-# 			  )
-# for ds in "${!datasetsAssoc[@]}"; do
-# $(BASE_COMMAND) -n 100 -o ${OUTSKIMDIR}${PREF}${ds} -i $SIGDIR${InputList[${ds}]} -x ${datasetsAssoc[${ds}]};
-# done
+### TT
+### Note: xsec from HTT http://cms.cern.ch/iCMS/user/noteinfo?cmsnoteid=CMS%20AN-2019/109
+### TT x section: 831.76 for inclusive sample, W->had 67,60% , W->l nu 3*10,8% = 32,4% (sum over all leptons)
+### hh = 45.7%, ll = 10.5%, hl = 21.9% (x2 for permutation t-tbar)
+DATA_MAP=( ["TT_fullyHad"]="377.96"
+				["TT_fullyLep"]="88.29"
+				["TT_semiLep"]="365.34"
+			  )
+for ds in ${!DATA_MAP[@]}; do
+	run_skim -n 100 -o ${OUTSKIM_DIR}${PREF}${ds} -i $SIG_DIR${IN_LIST[${ds}]} -x ${DATA_MAP[${ds}]}
+done
 
-# #### DY - xsec from https://twiki.cern.ch/twiki/bin/viewauth/CMS/SummaryTable1G25ns#DY_Z
-# $(BASE_COMMAND) -n 500 -o ${OUTSKIMDIR}${PREF}DYincl -i ${BKGDIR}${InputList["DY_incl"]} -x 6077.22
-# $(BASE_COMMAND) -n 700 -o ${OUTSKIMDIR}${PREF}DYmerged -i ${BKGDIR}${InputList["DY_merged"]} -g True --DY True -x 6077.22
-# datasets=("DY_1j" "DY_2j" "DY_4j" "DY_HT70To100" "DY_HT100To200" "DY_HT200To400" "DY_HT400To600" "DY_HT600To800" "DY_HT800To1200" "DY_HT1200To2500" "DY_HT2500ToInf");
-# for ds in "${datasets[@]}"; do
-# $(BASE_COMMAND) -n 300 -o ${OUTSKIMDIR}${PREF}"DYmerged" -i ${BKGDIR}${InputList[${ds}]} -g True --DY True -x 1.;
-# done
+### DY
+### Note: xsec from https://twiki.cern.ch/twiki/bin/viewauth/CMS/SummaryTable1G25ns#DY_Z
+run_skim -n 500 -o ${OUTSKIM_DIR}${PREF}DYincl -i ${BKG_DIR}${IN_LIST["DY_incl"]} -x 6077.22
+run_skim -n 700 -o ${OUTSKIM_DIR}${PREF}DYmerged -i ${BKG_DIR}${IN_LIST["DY_merged"]} -g True --DY True -x 6077.22
+DATA_LIST=("DY_1j" "DY_2j" "DY_4j" "DY_HT70To100" "DY_HT100To200" "DY_HT200To400" "DY_HT400To600" "DY_HT600To800" "DY_HT800To1200" "DY_HT1200To2500" "DY_HT2500ToInf");
+for ds in ${DATA_LIST[@]}; do
+	run_skim -n 300 -o ${OUTSKIM_DIR}${PREF}"DYmerged" -i ${BKG_DIR}${IN_LIST[${ds}]} -g True --DY True -x 1.
+done
 
-# $(BASE_COMMAND) -n 1000 -o ${OUTSKIMDIR}${PREF}DY_NLO -i ${BKGDIR}${InputList["DY_NLO"]} -x 6077.22
-# datasets=("DY_NLO_0j" "DY_NLO_1j" "DY_NLO_2j");
-# for ds in "${datasets[@]}"; do
-# $(BASE_COMMAND) -n 333 -o ${OUTSKIMDIR}${PREF}${ds} -i ${BKGDIR}${InputList[${ds}]} -x 1.;
-# done
+run_skim -n 1000 -o ${OUTSKIM_DIR}${PREF}DY_NLO -i ${BKG_DIR}${IN_LIST["DY_NLO"]} -x 6077.22
+DATA_LIST=("DY_NLO_0j" "DY_NLO_1j" "DY_NLO_2j");
+for ds in ${DATA_LIST[@]}; do
+	run_skim -n 333 -o ${OUTSKIM_DIR}${PREF}${ds} -i ${BKG_DIR}${IN_LIST[${ds}]} -x 1.
+done
 
-# datasets=("DY_NLO_PT50To100" "DY_NLO_PT100To250" "DY_NLO_PT250To400" "DY_NLO_PT400To650" "DY_NLO_PT650ToInf");
-# for ds in ${datasets[@]}; do
-# $(BASE_COMMAND) -n 200 -o ${OUTSKIMDIR}${PREF}${ds} -i ${BKGDIR}${InputList[${ds}]} -x 1.;
-# done
+DATA_LIST=("DY_NLO_PT50To100" "DY_NLO_PT100To250" "DY_NLO_PT250To400" "DY_NLO_PT400To650" "DY_NLO_PT650ToInf")
+for ds in ${DATA_LIST[@]}; do
+	run_skim -n 200 -o ${OUTSKIM_DIR}${PREF}${ds} -i ${BKG_DIR}${IN_LIST[${ds}]} -x 1.
+done
 
-# #### Wjets - filelists up to date
-# datasetsAssoc=( ["WJets_HT_0_70"]="-x 48917.48 -z 70"
-# 				["WJets_HT_70_100"]="-x 1362"
-# 				["WJets_HT_100_200"]="-x 1345"
-# 				["WJets_HT_200_400"]="-x 359.7"
-# 				["WJets_HT_400_600"]="-x 48.91"
-# 				["WJets_HT_600_800"]="-x 12.05"
-# 				["WJets_HT_800_1200"]="-x 5.501"
-# 				["WJets_HT_1200_2500"]="-x 1.329"
-# 				["WJets_HT_2500_Inf"]="-x 0.03216"
-# 			  )
-# for ds in ${!datasetsAssoc[@]}; do
-# 	$(BASE_COMMAND) -n 20 -o ${OUTSKIMDIR}${PREF}${ds} -i ${BKGDIR}${InputList[${ds}]} -y 1.213784 ${datasetsAssoc[${ds}]};
-# done
+#### Wjets - filelists up to date
+DATA_MAP=( ["WJets_HT_0_70"]="-x 48917.48 -z 70"
+				["WJets_HT_70_100"]="-x 1362"
+				["WJets_HT_100_200"]="-x 1345"
+				["WJets_HT_200_400"]="-x 359.7"
+				["WJets_HT_400_600"]="-x 48.91"
+				["WJets_HT_600_800"]="-x 12.05"
+				["WJets_HT_800_1200"]="-x 5.501"
+				["WJets_HT_1200_2500"]="-x 1.329"
+				["WJets_HT_2500_Inf"]="-x 0.03216"
+			  )
+for ds in ${!DATA_MAP[@]}; do
+	run_skim -n 20 -o ${OUTSKIM_DIR}${PREF}${ds} -i ${BKG_DIR}${IN_LIST[${ds}]} -y 1.213784 ${DATA_MAP[${ds}]}
+done
 
-# ##### ELECTROWEAK - XS Taken from HTT http://cms.cern.ch/iCMS/user/noteinfo?cmsnoteid=CMS%20AN-2019/109
-# ##### AND
-# ##### single top - XS Taken from HTT http://cms.cern.ch/iCMS/user/noteinfo?cmsnoteid=CMS%20AN-2019/109
-# datasetsAssoc=( ["EWKWPlus2Jets_WToLNu"]="25.62"
-# 				["EWKWMinus2Jets_WToLNu"]="20.25"
-# 				["EWKZ2Jets_ZToLL"]="3.987"
+### Electroweak
+### Note: xsec from HTT http://cms.cern.ch/iCMS/user/noteinfo?cmsnoteid=CMS%20AN-2019/109
+### Single Top
+### Note: xsec from HTT http://cms.cern.ch/iCMS/user/noteinfo?cmsnoteid=CMS%20AN-2019/109
+DATA_MAP=( ["EWKWPlus2Jets_WToLNu"]="25.62"
+				["EWKWMinus2Jets_WToLNu"]="20.25"
+				["EWKZ2Jets_ZToLL"]="3.987"
 
-# 				["ST_tW_antitop"]="35.85"
-# 				["ST_tW_top"]="35.85"
-# 				["ST_tchannel_antitop"]="80.95"
-# 				["ST_tchannel_top"]="136.02"
-# 			  )
-# for ds in "${!datasetsAssoc[@]}"; do
-# $(BASE_COMMAND) -n 50 -o ${OUTSKIMDIR}${PREF}${ds} -i ${BKGDIR}${InputList[${ds}]} -x ${datasetsAssoc[${ds}]};
-# done
+				["ST_tW_antitop"]="35.85"
+				["ST_tW_top"]="35.85"
+				["ST_tchannel_antitop"]="80.95"
+				["ST_tchannel_top"]="136.02"
+			  )
+for ds in ${!DATA_MAP[@]}; do
+	run_skim -n 50 -o ${OUTSKIM_DIR}${PREF}${ds} -i ${BKG_DIR}${IN_LIST[${ds}]} -x ${DATA_MAP[${ds}]}
+done
 
-### SM Higgs - from https://twiki.cern.ch/twiki/bin/view/LHCPhysics/CERNHLHE2019
-# HXSWG: xs(ZH) = 0.880 pb, xs(W+H) = 0.831 pb, xs(W-H) = 0.527 pb, xs(ggH) = 48.61 pb, xs(VBFH) = 3.766 pb, xs(ttH) = 0.5071 pb
-# Z->qq : 69.91% , Z->ll : 3,3658% (x3 for all the leptons), H->bb : 57.7%  , H->tautau : 6.32%
-# ZH (Zll, Hbb) : XSBD (xs ZH * BR Z) * H->bb, ZH (Zqq, Hbb) : XSBD (xs ZH * BR Z) * H->bb
-# ZH (Zall, Htautau) : XS teor ZH * BR H->tautau
-datasetsAssoc=( ["ggHTauTau"]="-n 30 -x 48.61 -y 0.0632"
+### SM Higgs
+### Note: from https://twiki.cern.ch/twiki/bin/view/LHCPhysics/CERNHLHE2019
+### HXSWG: xs(ZH) = 0.880 pb, xs(W+H) = 0.831 pb, xs(W-H) = 0.527 pb, xs(ggH) = 48.61 pb, xs(VBFH) = 3.766 pb, xs(ttH) = 0.5071 pb
+### Z->qq : 69.91% , Z->ll : 3,3658% (x3 for all the leptons), H->bb : 57.7%  , H->tautau : 6.32%
+### ZH (Zll, Hbb) : XSBD (xs ZH * BR Z) * H->bb, ZH (Zqq, Hbb) : XSBD (xs ZH * BR Z) * H->bb
+### ZH (Zall, Htautau) : XS teor ZH * BR H->tautau
+DATA_MAP=( ["ggHTauTau"]="-n 30 -x 48.61 -y 0.0632"
 				["VBFHTauTau"]="-n 30 -x 3.766 -y 0.0632"
 				["ZH_HTauTau"]="-n 30 -x 0.880 -y 0.0632"
 				["WplusHTauTau"]="-n 30 -x 0.831 -y 0.0632"
@@ -422,6 +411,6 @@ datasetsAssoc=( ["ggHTauTau"]="-n 30 -x 48.61 -y 0.0632"
 				["TTZZ"]="-n 20 -x 0.001386"
 				["TTWZ"]="-n 20 -x 0.00158"
 			  )
-for ds in "${!datasetsAssoc[@]}"; do
-	$(BASE_COMMAND) -o ${OUTSKIMDIR}${PREF}${ds} -i ${BKGDIR}${InputList[${ds}]} ${datasetsAssoc[${ds}]};
+for ds in ${!DATA_MAP[@]}; do
+	run_skim -o ${OUTSKIM_DIR}${PREF}${ds} -i ${BKG_DIR}${IN_LIST[${ds}]} ${DATA_MAP[${ds}]}
 done
