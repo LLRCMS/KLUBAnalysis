574ea988a7241fc4201fccd4899fe1a6b895028b
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-07-27 15:06:52 +0200

    improve code clarity

--- scripts/makeListOnStorage.py
+++ scripts/makeListOnStorage.py
@@ -1,114 +1,112 @@
-import os, sys
+import os
+from os.path import join
+import sys
+import warnings
+import argparse
+from argparse import ArgumentParser, RawTextHelpFormatter
 from subprocess import Popen, PIPE
+from tqdm import tqdm
 
+def create_dir(d):
+    if not os.path.exists(d):
+        os.makedirs(d)
+        
+def save_to_file(alist, filename, path_to_remove, mini_aod):
+    with open(filename, 'w') as f:
+        if not mini_aod:
+            for elem in alist:
+                f.write("{}\n".format(elem))
+        else:
+            f.write("FILELIST = cms.untracked.vstring()\n")
+            f.write("FILELIST.extend ([\n")
+            for elem in alist:
+                print(elem)
+                elem = elem.replace(path_to_remove, "")
 
+                f.write ("    '")
+                f.write("%s',\n" % elem) #vintage  
+            f.write("])\n")
+    print('Output stored in {}.'.format(filename))
 
-#tag = "Data2018A_Dec2019_resub_egamma"
-#tag = "Data2018ABC_Dec2019"
-#tag = "Data2018D_Dec2019"
-#tag = "Data2018D_Dec2019_resub"
-#tag = "Data2018D_Dec2019_resub_egamma"
-#tag = "MC2018_Dec2019"
-#tag = "MC2018_Dec2019_resub"
-tag = "MC2018_Dec2019_resub2"
-#tag = "Signals2018_Dec2019"
+def make_input_lists(args):
+    leaf_dir = args.data_period + '_' + args.kind
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    out_dir  = os.path.normpath(join(this_dir, '..', 'inputFiles', leaf_dir))
+    create_dir(out_dir)
 
-#outFolder = '/home/llr/cms/amendola/HHLegacy/CMSSW_10_2_16/src/KLUBAnalysis/inputFiles/Legacy2018_data/'
-outFolder = '/home/llr/cms/amendola/HHLegacy/CMSSW_10_2_16/src/KLUBAnalysis/inputFiles/Legacy2018_backgrounds/'
-#outFolder = '/home/llr/cms/amendola/HHLegacy/CMSSW_10_2_16/src/KLUBAnalysis/inputFiles/Legacy2018_signals/'
-
-areEnrichedMiniAOD = False; # if true:  add a header and the /store.. etc to run ntuplizer on Tier3 on CMSSW
-                                 # if false: only add the polgrid server to run the skim and submit on root
-# ====================================================================
-
-def formatName (name, pathToRem):
-    name.strip(pathToRem)
-    name = "root://polgrid4.in2p3.fr/" + name
-    return name
-
-def saveToFile (lista, filename, areEnrichedMiniAOD):
-    f = open (filename, "w")
-
-    if not areEnrichedMiniAOD:
-        for elem in lista:
-            f.write("%s\n" % elem) #vintage
+    xrd_map = dict(llr='root://polgrid4.in2p3.fr/',
+                   uhh='root://dcache-cms-xrootd.desy.de:1094/')
+    dpm_home = '/dpm/in2p3.fr/home/cms/trivcat'
+    store    = 'store/user/lportale/HHNtuples_res/UL18/'
+    path     = join(dpm_home, store, args.tag)
     
-    else:
-        f.write("FILELIST = cms.untracked.vstring()\n")
-        f.write("FILELIST.extend ([\n")
-        for elem in lista:
-            elem = elem.replace ("root://polgrid4.in2p3.fr//dpm/in2p3.fr/home/cms/trivcat", "")
-            f.write ("    '")
-            f.write("%s',\n" % elem) #vintage  
-        f.write("])\n")
- 
-    f.close()
+    rfdir  = '/usr/bin/rfdir'
+    awk    = "| awk '{{print $9}}'"
+    rfcomm = lambda s : rfdir + ' {} '.format(s) + awk
+    pipe   = Popen(rfcomm(path), shell=True, stdout=PIPE)
+     
+    all_lists = {}
+     
+    for line in tqdm(pipe.stdout):
+        smpl_path = join(path, line).strip()    
+        smpl_name = line
+        all_lists[smpl_name] = []
+        
+        for _ in range(3): #smpl_name, tag, hash subfolders
+            comm = rfcomm(smpl_path.strip())
+            out = Popen(comm, shell=True, stdout=PIPE).stdout.readlines()
+            nlines = len(out)
+            if nlines > 0:
+                if nlines > 1:
+                    mes = 'In {} there are too many subfolder. '.format(smpl_path)
+                    mes += 'Using the last one (most recent submission).'
+                    warnings.warn(mes)   
+                smpl_path = join(smpl_path, out[-1].strip())
 
+        # loop over the folders 0000, 1000, etc...
+        comm = rfcomm(' {} '.format(smpl_path.strip()))
+        out = Popen(comm, shell=True, stdout=PIPE).stdout.readlines()
+        for subfold in out:
+            final_dir = join(smpl_path, subfold.strip())
+            str_comm = '{} | grep ' + ( 'Enriched_miniAOD'
+                                        if args.mini_aod else 'HTauTauAnalysis' )
+            file_comm = rfcomm( str_comm.format(final_dir.strip()) )
 
-# ====================================================================
+            out_files = Popen(file_comm, shell=True, stdout=PIPE).stdout.readlines()
+            for filename in out_files:
+                name = xrd_map[args.xrd_door] + join(final_dir, filename.strip())
+                all_lists[smpl_name].append(name)
+     
+    # save file lists
+    for sample, alist in all_lists.items():
+        if alist: 
+            ext = '.py' if args.mini_aod else '.txt'
+            out_name = join(out_dir, sample.strip() + ext)
+            save_to_file(alist=alist, filename=out_name,
+                         path_to_remove=join(xrd_map[args.xrd_door], dpm_home.lstrip(os.sep)),
+                         mini_aod=args.mini_aod)
+        else:
+            warnings.warn('Folder for dataset {} is empty'.format(sample))
 
-useOnly = [] #empty list to do list fot all the folders
+if __name__ == "__main__":
+    usage = 'Produces lists with input files for skim jobs. \n'
+    usage += 'Run example: `python scripts/makeListOnStorage.py -t MC_Jul2022 -d UL18 -k Data`.'
+    parser = ArgumentParser(description=usage, formatter_class=RawTextHelpFormatter)
+    parser.add_argument('-t', '--tag', dest='tag', required=True, type=str,
+                        help='Tag used for the input big ntuples')
+    help1 = ( 'if true:  add a header and the /store.. etc to run ntuplizer ' +
+              'on Tier3 on CMSSW; if false: only add the polgrid server to run ' +
+              'the skim and submit on root' )
+    parser.add_argument('--with_mini_aod', dest='mini_aod',
+                        required=False, default=0, type=int,
+                        help=help1)
+    parser.add_argument('-d', '--data_period', dest='data_period',
+                        required=True, type=str,
+                        help='Which data period to choose: Legacy2018, UL18, ...')
+    parser.add_argument('-k', '--kind', dest='kind', choices=['Data', 'MC'],
+                        required=True, type=str, help='Data or MC')
+    parser.add_argument('-x', '--xrd_door', dest='xrd_door', default='llr', 
+                        required=False, type=str, help='Data or MC')
 
-print useOnly
-
-
-dpmhome = "/dpm/in2p3.fr/home/cms/trivcat"
-
-partialPath = "/store/user/camendol/HHNtuples2018Legacy/"
-
-
-path = dpmhome + partialPath + tag
-if outFolder[-1] != "/": outFolder += '/'
-
-command = "/usr/bin/rfdir %s | awk '{print $9}'" % path
-#print command
-pipe = Popen(command, shell=True, stdout=PIPE)
-
-allLists = {} #dictionary
-
-for line in pipe.stdout:
-    
-    #print line
-    if useOnly:
-        if not (line.strip()) in useOnly:
-            continue
-    samplesPath = (path + "/" + line).strip()    
-    sampleName = line
-    allLists[sampleName] = []
-    print sampleName.strip()
-    for level in range(0, 3): #sampleName, tag, hash subfolders
-        comm = "/usr/bin/rfdir %s | awk '{print $9}'" % samplesPath.strip()
-        #print "comm: ==> " , comm
-        pipeNested = Popen (comm, shell=True, stdout=PIPE)
-        out = pipeNested.stdout.readlines()
-        numLines = len (out)
-        if numLines > 0 :
-            if numLines > 1 : print "  *** WARNING: In %s too many subfolders, using last one (most recent submission)" % samplesPath        
-            samplesPath = samplesPath + "/" + out[-1].strip()
-    #print samplesPath
-    # now I have to loop over the folders 0000, 1000, etc...
-    comm = "/usr/bin/rfdir %s | awk '{print $9}'" % samplesPath.strip()
-    pipeNested = Popen (comm, shell=True, stdout=PIPE)
-    out = pipeNested.stdout.readlines()
-    for subfol in out:
-        finalDir = samplesPath + "/" + subfol.strip()
-        getFilesComm = ""
-        if areEnrichedMiniAOD :
-            getFilesComm = "/usr/bin/rfdir %s | grep Enriched_miniAOD | awk '{print $9}'" % finalDir.strip()
-        else :
-            getFilesComm = "/usr/bin/rfdir %s | grep HTauTauAnalysis | awk '{print $9}'" % finalDir.strip()
-            #print getFilesComm
-        pipeGetFiles = Popen (getFilesComm, shell=True, stdout=PIPE)
-        outGetFiles = pipeGetFiles.stdout.readlines()
-        for filename in outGetFiles:
-            name = formatName (finalDir + "/" + filename.strip(), dpmhome)
-            allLists[sampleName].append (name)
-
-# now I have all file lists, save them
-for sample, lista in allLists.iteritems():
-   if lista: 
-      outName = outFolder + sample.strip()+".txt"
-      if areEnrichedMiniAOD : outName = outFolder + sample.strip()+".py"
-      saveToFile (lista, outName, areEnrichedMiniAOD)
-   else:
-      print "  *** WARNING: Folder for dataset %s is empty" % sample
+    FLAGS = parser.parse_args()
+    make_input_lists(FLAGS)
