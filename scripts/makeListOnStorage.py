0223a93fa8aa6c65c21b11b9f40ab331d5e2486f
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-08-03 17:53:12 +0200

    :fire: remove save file option and mini_aod flag

--- scripts/makeListOnStorage.py
+++ scripts/makeListOnStorage.py
@@ -14,21 +14,6 @@ def create_dir(d):
     if not os.path.exists(d):
         os.makedirs(d)
         
-def save_to_file(alist, filename, path_to_remove, mini_aod):
-    with open(filename, 'w') as f:
-        if not mini_aod:
-            for elem in alist:
-                f.write("{}\n".format(elem))
-        else:
-            f.write('FILELIST = cms.untracked.vstring()\n')
-            f.write('FILELIST.extend ([\n')
-            for elem in alist:
-                print(elem)
-                elem = elem.replace(path_to_remove, '')
-                f.write("     '{}',\n".format(elem))
-            f.write("])\n")
-    print('{} output stored in {}.'.format(os.path.basename(__file__), filename))
-
 def make_input_lists(args):
     xrd_door = {'llr': 'root://polgrid4.in2p3.fr/',
                 'uhh': 'root://dcache-cms-xrootd.desy.de:1094/'}[args.xrd_door]
@@ -74,8 +59,7 @@ def make_input_lists(args):
         out = Popen(comm, shell=True, stdout=PIPE).stdout.readlines()
         for subfold in out:
             final_dir = join(smpl_path, subfold.strip())
-            str_comm = '{} | grep ' + ( 'Enriched_miniAOD'
-                                        if args.mini_aod else 'HTauTauAnalysis' )
+            str_comm = '{} | grep HTauTauAnalysis'
             file_comm = rfcomm( str_comm.format(final_dir.strip()) )
 
             out = Popen(file_comm, shell=True, stdout=PIPE).stdout.readlines()
@@ -87,15 +71,8 @@ def make_input_lists(args):
         mes = 'Sample {} was not found in {}.'.format(args.sample, path)
         raise RuntimeError(mes)
     
-    # save file lists
     for sample, alist in all_lists.items():
-        if alist: 
-            ext = '.py' if args.mini_aod else '.txt'
-            out_name = join(out_dir, sample.strip() + ext)
-            save_to_file(alist=alist, filename=out_name,
-                         path_to_remove=join(xrd_door, dpm_home.lstrip(os.sep)),
-                         mini_aod=args.mini_aod)
-        else:
+        if not alist: 
             warnings.warn('Folder for dataset {} is empty'.format(sample))
 
 if __name__ == "__main__":
@@ -104,12 +81,6 @@ if __name__ == "__main__":
     parser = ArgumentParser(description=usage, formatter_class=RawTextHelpFormatter)
     parser.add_argument('-t', '--tag', dest='tag', required=True, type=str,
                         help='Tag used for the input big ntuples')
-    help_mini_aod = ( 'if true:  add a header and the /store.. etc to run ' +
-                      'ntuplizer on Tier3 on CMSSW; if false: only add ' +
-                      'the polgrid server to run the skim and submit on root' )
-    parser.add_argument('--with_mini_aod', dest='mini_aod',
-                        required=False, default=0, type=int,
-                        help=help_mini_aod)
     parser.add_argument('-d', '--data_period', dest='data_period', required=True, type=str,
                         help='Which data period to choose: Legacy2018, UL18, ...')
     parser.add_argument('-x', '--xrd_door', dest='xrd_door', default='llr',
