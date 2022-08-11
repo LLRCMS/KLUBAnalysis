ed68a8227b9fbf7d94fba46be57fefa65eec3034
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-08-11 17:40:32 +0200

    :fire: remove forgotten exit

--- scripts/submit_skims_UL18.sh
+++ scripts/submit_skims_UL18.sh
@@ -224,7 +224,7 @@ for ds in ${DATA_LIST[@]}; do
 		fi
 	done
 done
-exit 1
+
 ### Run on HH resonant signal samples
 DATA_LIST=( "GluGluToRad" "GluGluToBulkGrav" "VBFToRad" "VBFToBulkGrav" )
 MASSES=("250" "260" "270" "280" "300" "320" "350" "400" "450" "500" "550" "600" "650" "700" "750" "800" "850" "900" "1000" "1250" "1500" "1750" "2000" "2500" "3000")
