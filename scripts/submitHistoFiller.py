c1f1d068fd8d84932d1991e805f9daee8efa0d34
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-08-12 14:40:50 +0200

    histo filler: tag from command line

--- scripts/submitHistoFiller.py
+++ scripts/submitHistoFiller.py
@@ -8,6 +8,9 @@ hourdate = datetime.datetime.now().strftime('%Y.%m.%d_%H.%M.%S').replace('.','-'
 parser = argparse.ArgumentParser(description='Command line parser of plotting options')
 parser.add_argument('--cfg', dest='cfg', required=True,
                     help='name of the main cfg to run', default=None)
+parser.add_argument('-o', '--outdir', dest='outdir', required=False,
+                    default=os.path.join('/data_CMS/cms/', os.environ['USER'], 'HHresonant_hist'),
+                    help='name of working space (defaults to timestamp)', )
 parser.add_argument('-t', '--tag', dest='tag', required=False,
                     default='HistoFiller_'+hourdate,
                     help='name of working space (defaults to timestamp)', )
@@ -15,21 +18,24 @@ parser.add_argument('--njobs', dest='njobs', required=False, type=int,
                     help='number of jobs for parallelization', default=10)
 FLAGS = parser.parse_args()
 
-outdir     = 'filljobs_' + FLAGS.tag
 exe        = 'testAnalysisHelper.exe'
 scriptname = 'filler.sh'
-logname    = 'log_${1}.txt'
 
 def create_dir(d):
     if not os.path.exists(d):
         os.makedirs(d)
-create_dir(outdir)
+    else:
+        mes = 'Folder {d} already exists. You may want to remove it ith `rm -r {d}`'.format(d=d)
+        raise ValueError(mes)
 
-scriptpath = os.path.join(outdir, scriptname)
+tagdir = os.path.join(FLAGS.outdir, FLAGS.tag)
+create_dir(tagdir)
+
+scriptpath = os.path.join(tagdir, scriptname)
 scriptpath_condor = scriptpath.replace('.sh','.condor')
 with open(scriptpath, 'w') as s:
     command = (exe + ' ' + FLAGS.cfg + ' ${1}' + ' ' +
-               str(FLAGS.njobs) + ' ' + FLAGS.tag)
+               str(FLAGS.njobs) + ' ' + tagdir)
     s.write('\n'.join(('#!/bin/bash',
                         'export X509_USER_PROXY=~/.t3/proxy.cert',
                         'source /cvmfs/cms.cern.ch/cmsset_default.sh',
@@ -37,18 +43,18 @@ with open(scriptpath, 'w') as s:
                         'export SCRAM_ARCH=slc6_amd64_gcc491',
                         'eval `scram r -sh`',
                         'source scripts/setup.sh',
-                        command)))
-    os.system('chmod u+rwx ' + os.path.join(outdir, scriptname))
+                        command + '\n')))
+    os.system('chmod u+rwx ' + os.path.join(tagdir, scriptname))
 
-condlog = os.path.join(outdir, 'outputs')
+condlog = os.path.join(tagdir, 'logs')
 create_dir(condlog)
 with open(scriptpath_condor, 'w') as s:
     s.write('\n'.join(('Universe = vanilla',
                        'Executable = {}'.format(scriptpath),
                        'input = /dev/null',
-                       'output = {}/condor_log_$(Process).out'.format(condlog),
-                       'error  = {}/condor_log_$(Process).err'.format(condlog),
-                       'log  = {}/condor_log_$(Process).log'.format(condlog),
+                       'output = {}/$(Process).out'.format(condlog),
+                       'error  = {}/$(Process).err'.format(condlog),
+                       'log  = {}/$(Process).log'.format(condlog),
                        'getenv = true',
                        'should_transfer_files = YES',
                        '',
@@ -56,6 +62,7 @@ with open(scriptpath_condor, 'w') as s:
                        'WNTag=el7',
                        '+SingularityCmd = ""',
                        'include : /opt/exp_soft/cms/t3_tst/t3queue |',
+                       '',
                        'Arguments = $(Process) ',
                        'queue {}'.format(FLAGS.njobs))))
     
