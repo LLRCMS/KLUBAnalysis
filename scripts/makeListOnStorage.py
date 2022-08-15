36bd5ab68f0612bd9dfe73982c9d0c3be52bfaa2
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-08-15 09:54:47 +0200

    add missing piece to create input files list on-the-fly

--- scripts/makeListOnStorage.py
+++ scripts/makeListOnStorage.py
@@ -1,9 +1,9 @@
 # coding: utf-8
 
-_all_ = [ "make_input_lists" ]
+_all_ = [ 'make_input_lists' ]
 
 import os
-from os.path import join
+from os import path as op
 import sys
 import warnings
 import argparse
@@ -11,22 +11,27 @@ from argparse import ArgumentParser, RawTextHelpFormatter
 from subprocess import Popen, PIPE
 
 def create_dir(d):
-    if not os.path.exists(d):
+    if not op.exists(d):
         os.makedirs(d)
         
 def make_input_lists(args):
     xrd_door = {'llr': 'root://polgrid4.in2p3.fr/',
-                'uhh': 'root://dcache-cms-xrootd.desy.de:1094/'}[args.xrd_door]
+                'uhh': 'root://dcache-cms-xrootd.desy.de:1094/',
+                'mib': '???'}[args.institute]
 
-    leaf_dir = args.data_period + '_' + args.kind
-    this_dir = os.path.dirname(os.path.abspath(__file__))
-    out_dir  = os.path.normpath(join(this_dir, '..', 'inputFiles', leaf_dir))
-    create_dir(out_dir)
+    store = {'llr': 'store/user/lportale/HHNtuples_res',
+             'uhh': '???',
+             'mib': '???'}[args.institute]
+    store    = op.join(store, args.data_period)
 
     dpm_home = '/dpm/in2p3.fr/home/cms/trivcat'
-    store    = os.path.join('store/user/lportale/HHNtuples_res', args.data_period)
     prefix   = 'Data' if args.kind == 'Data' else 'MC'
-    path     = join(dpm_home, store, prefix + '_' + args.tag)
+    path     = op.join(dpm_home, store, prefix + '_' + args.tag)
+    
+    leaf_dir = args.data_period + '_' + args.kind
+    this_dir = op.dirname(op.abspath(__file__))
+    out_dir  = op.normpath( op.join(op.dirname(this_dir), 'inputFiles', leaf_dir) )
+    create_dir(out_dir)
     
     rfdir  = '/usr/bin/rfdir'
     awk    = "| awk '{{print $9}}'"
@@ -36,7 +41,7 @@ def make_input_lists(args):
     all_lists = {}
      
     for smpl_name in pipe.stdout:
-        smpl_path = join(path, smpl_name).strip()    
+        smpl_path = op.join(path, smpl_name).strip()    
         if args.sample not in smpl_name and args.sample != 'all':
             continue
 
@@ -52,21 +57,23 @@ def make_input_lists(args):
                     mes = 'In {} there are too many subfolder. '.format(smpl_path)
                     mes += 'Using the last one (most recent submission).'
                     warnings.warn(mes)   
-                smpl_path = join(smpl_path, out[-1].strip())
+                smpl_path = op.join(smpl_path, out[-1].strip())
 
         # loop over the folders 0000, 1000, etc...
         comm = rfcomm(' {} '.format(smpl_path.strip()))
         out = Popen(comm, shell=True, stdout=PIPE).stdout.readlines()
         for subfold in out:
-            final_dir = join(smpl_path, subfold.strip())
+            final_dir = op.join(smpl_path, subfold.strip())
             str_comm = '{} | grep HTauTauAnalysis'
             file_comm = rfcomm( str_comm.format(final_dir.strip()) )
 
             out = Popen(file_comm, shell=True, stdout=PIPE).stdout.readlines()
             for filename in out:
-                name = xrd_door + join(final_dir, filename.strip())
+                name = xrd_door + op.join(final_dir, filename.strip())
                 all_lists[smpl_name].append(name)
 
+    assert len(all_lists.keys()) == 1 # currently calling this script once per sample
+    
     if not all_lists:
         mes = 'Sample {} was not found in {}.'.format(args.sample, path)
         raise RuntimeError(mes)
@@ -74,6 +81,10 @@ def make_input_lists(args):
     for sample, alist in all_lists.items():
         if not alist: 
             warnings.warn('Folder for dataset {} is empty'.format(sample))
+        else:
+            with open(op.join(out_dir, args.sample+'.txt'), 'w') as f:
+                for l in alist:
+                    f.write(l + '\n')
 
 if __name__ == "__main__":
     usage = 'Produces lists with input files for skim jobs. \n'
@@ -83,7 +94,7 @@ if __name__ == "__main__":
                         help='Tag used for the input big ntuples')
     parser.add_argument('-d', '--data_period', dest='data_period', required=True, type=str,
                         help='Which data period to choose: Legacy2018, UL18, ...')
-    parser.add_argument('-x', '--xrd_door', dest='xrd_door', default='llr',
+    parser.add_argument('-x', '--institute', dest='institute', default='llr',
                         choices=('llr', 'uhh', 'mib'),
                         required=False, type=str, help='Which set of machines to use: LLR, Hamburg or Milano.')
     help_sample = ( 'For which sample to create the input list.\n' +
