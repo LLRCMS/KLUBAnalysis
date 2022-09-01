1b347945a21db8d563b9705aaf10bb4198a820ea
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-09-01 09:54:32 +0200

    :bug: simplify and ensure LD_LIBRARY_PATH is defined

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,43 +1,16 @@
 #!/usr/bin/env bash
 
-## THIS TO CHECK ON LD_LYBRARY_PATH: will work on screen but leave previous path in the config
+THISFILE="$( [ ! -z "$ZSH_VERSION" ] && echo "${(%):-%x}" || echo "${BASH_SOURCE[0]}" )"
+KLUBDIR="$( cd "$( dirname "$THISFILE" )/.." && pwd )"
+CMSSWDIR="$( cd "$( dirname "$THISFILE" )/../.." && pwd )"
 
-# paths=$(echo $LD_LIBRARY_PATH | tr ":" "\n") # split on :
-# pathunset=1
-# for pp in $paths
-# do
-#     if [[ $pp == *"`pwd`/lib"* ]] ; then
-#         echo "Warning: path to KLUB binaries already set to: $pp"
-#         echo "         ... Ignoring this setup command"
-#         pathunset=0
-#     fi
-# done
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${KLUBDIR}/lib"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${CMSSWDIR}/HHKinFit2"
 
-# paths=$(echo $PATH | tr ":" "\n") # split on :
-pathunset=1
-# for pp in $paths 
-# do
-#     if [[ $pp == *KLUBAnalysis/bin ]] ; then
-#         echo "Warning: path to KLUB binaries already set to: $pp"
-#         echo "         ... Ignoring this setup command"
-#         pathunset=0
-#         break
-#     fi
-# done
-
-if [ ${pathunset} -eq 0 ] ; then
-    THISFILE="$( [ ! -z "$ZSH_VERSION" ] && echo "${(%):-%x}" || echo "${BASH_SOURCE[0]}" )"
-    KLUBDIR="$( cd "$( dirname "$THISFILE" )/.." && pwd )"
-    CMSSWDIR="$( cd "$( dirname "$THISFILE" )/../.." && pwd )"
-	echo $KLUBDIR
-	
-    export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${KLUBDIR}/lib"
-    export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${CMSSWDIR}/HHKinFit2"
-
-    if [ -n "${DYLD_LIBRARY_PATH}" ]; then
-        export DYLD_LIBRARY_PATH="${DYLD_LIBRARY_PATH}:${KLUBDIR}/lib"
-        export DYLD_LIBRARY_PATH="${DYLD_LIBRARY_PATH}:${CMSSWDIR}/HHKinFit2"
-    fi
-
-    export PATH="${PATH}:${KLUBDIR}/bin"
+if [ -n "${DYLD_LIBRARY_PATH}" ]; then
+    export DYLD_LIBRARY_PATH="${DYLD_LIBRARY_PATH}:${KLUBDIR}/lib"
+    export DYLD_LIBRARY_PATH="${DYLD_LIBRARY_PATH}:${CMSSWDIR}/HHKinFit2"
 fi
+
+export PATH="${PATH}:${KLUBDIR}/bin"
+
