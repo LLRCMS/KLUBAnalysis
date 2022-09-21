e937b155ddaf8be1962d7f43bf465e9ab07905ba
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-09-21 11:14:59 +0200

    cap number of intersections to the maximum

--- src/TriggerSF.cc
+++ src/TriggerSF.cc
@@ -275,7 +275,7 @@ auto TriggerSF::mGetTriggerIntersections( const TriggerChannelLists& list,
   VectorCombinations comb;
   vec<std::string> strs = list.get_full(chn, isData);
   vec<std::string> new_strs = mKLUBStandaloneNameMatching(strs, chn, isData); //KLUB - Python standalone matching
-  vec<std::string> res = comb.combine_all_k<std::string>(new_strs, 4).flatten(mTriggerStrConnector);
+  vec<std::string> res = comb.combine_all_k<std::string>(new_strs, new_strs.size()).flatten(mTriggerStrConnector);
   // comb.combine_all_k<std::string>(strs).print();
   return res;                                                                     
 }
