6951909fa8e828cd87b815b12bad05f08e639f35
Johan Wulff <jwulff@lip.pt>
2022-11-10 14:21:55 +0100

    added louis' suggestions for submitHistoFiller.py

--- scripts/submitHistoFiller_lxplus.py
+++ scripts/submitHistoFiller_lxplus.py
@@ -8,15 +8,16 @@ write = lambda stream, text: stream.write(text + '\n')
 
 def make_parser():
     parser = argparse.ArgumentParser(description='Command line parser of plotting options')
-    parser.add_argument('--outdir', type=str, dest="outdir", required=True,
+    parser.add_argument('--submit_dir', type=str, dest="submit_dir", required=True,
                         help='/afs/directory/to/submit/from. Must be afs\
     because condor submission is only allowed from /afs on lxplus.')
     parser.add_argument('--cfg', dest='cfg', required=True,
                         help='name of the main cfg to run', default=None)
-    parser.add_argument('--tag', dest='tag', required=True, default=None,
+    parser.add_argument('--outdir', dest='outdir', required=True, default=None,
                         help='name of working space')
     parser.add_argument('--njobs', dest='njobs', required=False, type=int,
-                        help='number of jobs for parallelization', default=10)
+                        help='number of jobs for parallelization', default=20)
+    parser.add_argument('--dry', '-d', dest='dry', action='store_true')
     return parser
 
 
@@ -54,7 +55,7 @@ def write_submit_file(submit_file, executable, out_dir, err_dir):
         write(s,'queue') 
 
 
-def write_postscript(postscript, tag,):
+def write_postscript(postscript, outdir,):
     with open(postscript, 'w') as ps:
         write(ps,'#!/bin/bash')
         write(ps,'export X509_USER_PROXY=~/Condor/x509up_u149480')
@@ -63,10 +64,10 @@ def write_postscript(postscript, tag,):
         write(ps,'export SCRAM_ARCH=slc6_amd64_gcc491')
         write(ps,'eval `scram r -sh`')
         write(ps,'source scripts/setup.sh')
-        write(ps,'cd {} || exit 1'.format(tag))
+        write(ps,'cd {} || exit 1'.format(outdir))
         write(ps,'hadd outPlotter.root *.root')
         write(ps,'cd - || exit 1')
-        write(ps,'python scripts/combineFillerOutputs.py --dir {}'.format(tag))
+        write(ps,'python scripts/combineFillerOutputs.py --dir {}'.format(outdir))
         os.system('chmod u+rwx ' + postscript)
 
 
@@ -91,57 +92,64 @@ def write_dagfile(dagfile, njobs, submit_file, postscript_submit):
 
 #hourdate = datetime.datetime.now().strftime('%Y.%m.%d_%H.%M.%S').replace('.','-')
 
-def main():
-    parser = make_parser()
-    args = parser.parse_args()
-    outdir = args.outdir
-    exe = 'testAnalysisHelper.exe'
-    condor_executable = 'filler.sh'
-
-    # check if outdir exists
-    if os.path.exists(outdir):
-        print("{} already exists. Checking if it's empty.".format(outdir))
-        contents = os.listdir(outdir)
+def main(submit_dir,
+         cfg,
+         outdir,
+         njobs,
+         executable='testAnalysisHelper.exe',
+         condor_executable='filler.sh',
+         dry=False):
+    # check if submit_dir exists
+    if os.path.exists(submit_dir):
+        print("{} already exists. Checking if it's empty.".format(submit_dir))
+        contents = os.listdir(submit_dir)
         if len(contents) > 0:
-            print("{} contains the following :".format(outdir))
+            print("{} contains the following :".format(submit_dir))
             for i in contents:
                 print(i)
-            print("Please remove these or use a different tag!")
+            print("Please remove these or use a different outdir!")
             sys.exit(1)
         
-    create_dir(outdir)
-    condor_out = os.path.join(outdir, 'out')
+    create_dir(submit_dir)
+    condor_out = os.path.join(submit_dir, 'out')
     create_dir(condor_out)
-    condor_err = os.path.join(outdir, 'err')
+    condor_err = os.path.join(submit_dir, 'err')
     create_dir(condor_err)
 
-    scriptpath = os.path.join(outdir, condor_executable)
+    scriptpath = os.path.join(submit_dir, condor_executable)
     write_filler_executable(scriptpath=scriptpath,
-                            exe=exe,
-                            cfg=args.cfg,
-                            njobs=args.njobs)
-    submit_file_path = os.path.join(outdir, 'filler.submit')
+                            exe=executable,
+                            cfg=cfg,
+                            njobs=njobs)
+    submit_file_path = os.path.join(submit_dir, 'filler.submit')
     write_submit_file(submit_file=submit_file_path,
                       executable=scriptpath,
                       out_dir=condor_out,
                       err_dir=condor_err)
-    postscript_path = os.path.join(outdir, 'postscript.sh')
-    write_postscript(postscript=postscript_path, tag=args.tag)
-    postscript_submit_path = os.path.join(outdir, 'postscript.submit')
+    postscript_path = os.path.join(submit_dir, 'postscript.sh')
+    write_postscript(postscript=postscript_path, outdir=outdir)
+    postscript_submit_path = os.path.join(submit_dir, 'postscript.submit')
     write_postscript_submit(submit_file=postscript_submit_path,
                             executable=postscript_path,
                             out_dir=condor_out,
                             err_dir=condor_err)
-    dagfile = os.path.join(outdir, 'filler.dag')
+    dagfile = os.path.join(submit_dir, 'filler.dag')
     write_dagfile(dagfile=dagfile,
-                  njobs=args.njobs,
+                  njobs=njobs,
                   submit_file=submit_file_path,
                   postscript_submit=postscript_submit_path)
+    if not dry:
+        os.chdir(submit_dir)
+        launch_command = 'condor_submit_dag {}'.format(dagfile)
+        print('The following command was run: {}'.format(launch_command))
+        os.system(launch_command)
 
 
 if __name__ == "__main__":
-    main()
-    #os.chdir(condor_dir)
-    #launch_command = 'condor_submit {}'.format(scriptpath_condor)
-    #print('The following command was run: {}'.format(launch_command))
-    #os.system(launch_command)
+    parser = make_parser()
+    args = parser.parse_args()
+    main(submit_dir=args.submit_dir,
+         cfg=args.cfg,
+         outdir=args.outdir,
+         njobs=args.njobs,
+         dry=args.dry)
