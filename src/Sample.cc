5d5d14cee1064456ad3c5b2728df54459d8a112d
Bruno Alves <balves16@hotmail.com>
2023-09-27 15:15:32 +0200

    add missing support for reading resubmitted skims

--- src/Sample.cc
+++ src/Sample.cc
@@ -5,132 +5,88 @@
 
 using namespace std;
 
-// Sample::Sample (string name, string treename)
-// {
-//     name_ = name;
-//     tree_ = new TChain (treename);
-//     filelistname_ = "";
-//     tree_ = 0;
-//     eff_  = 0.;
-//     evt_num_ = 0.;
-//     evt_den_ = 0.;
-//     nentries_ = 0.;
-// }
-
-Sample::Sample (string name, string filelistname, string treename, string histoname, int binEffDen)
-// Sample (name, treename)
+Sample::Sample(string name, string filelistname, string treename, string histoname, int binEffDen):
+  name_(name), eff_(0.), evt_num_(0.), evt_den_(0.), nentries_(0.), treename_(treename), histoname_(histoname), bin_eff_den_(binEffDen)
 {
-  name_ = name;
   tree_ = new TChain (treename.c_str());
-  //tree_ = unique_ptr<TChain>(new TChain(treename.c_str()));
-  filelistname_ = filelistname;
-  eff_         = 0.;
-  evt_num_     = 0.;
-  evt_den_     = 0.;
-  nentries_    = 0.;
-  bin_eff_den_ = binEffDen;
-  treename_    = treename;
-  histoname_   = histoname;
+  filelistname_.push_back(filelistname);
 }
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+Sample::Sample(string name, std::vector<std::string> filelistname, string treename, string histoname, int binEffDen):
+  name_(name), eff_(0.), evt_num_(0.), evt_den_(0.), nentries_(0.), bin_eff_den_(binEffDen), treename_(treename), histoname_(histoname)
+{
+  tree_ = new TChain (treename.c_str());
+  filelistname_ = filelistname;
+}
 
 Sample::~Sample ()
 {
   // delete tree_;
 }
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-// Sample::Sample(std::string name, std::vector<Sample const *> sampleList)
-// {
-//     name_ = name;
-
-//     // all the following are set to dummy values
-//     tree_ = nullptr;
-//     filelistname_ = "";
-//     eff_         = 0.;
-//     evt_num_     = 0.;
-//     evt_den_     = 0.;
-//     nentries_    = 0.;
-//     bin_eff_den_ = -1;
-//     treename_    = "";
-//     histoname_   = "";
-
-//     if (sampleList.size() == 0)
-//     {
-//         cerr << "*** Sample::Sample (ctor from list of samples) : ERROR : the input list is empty, will create and empty sample" << endl;
-//         return;
-//     }
-
-//     cout << "@ Creating sample " << name_ << " summing the following samples" << endl;
-//     for (auto s : sampleList)
-//         cout << "  ---> " << s->getName() << endl;
-
-//     // clone the structure and histos from sample number 1
-// }
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 bool Sample::openFileAndTree()
 {
-  cout << "@ Opening sample: " << name_ << endl;
-  cout << "  ---> " << filelistname_ << endl;
-
-  ifstream fList (filelistname_);
-  if (!fList.good())
-  {
-    cerr << "*** Sample::openFileAndTree : ERROR : could not open file " << filelistname_ << endl;
-    return false;
-    // cerr << " ===> Terminating execution" << endl;
-    // assert(0);
-  }
-  string line;
   int counter = 0;
-  while (std::getline(fList, line))
-  {
-    line = line.substr(0, line.find("#", 0)); // remove comments introduced by #
-    //cout << line << endl;
-    while (line.find(" ") != std::string::npos) line = line.erase(line.find(" "), 1); // remove white spaces
-    while (line.find("\n") != std::string::npos) line = line.erase(line.find("\n"), 1); // remove new line characters
-    while (line.find("\r") != std::string::npos) line = line.erase(line.find("\r"), 1); // remove carriage return characters
-    if (!line.empty()) // skip empty lines
-    {
-      ++counter;
-      tree_->Add(line.c_str());
+  std::cout << "@ Opening sample(s): " << name_ << std::endl;
+  for(std::string fn : filelistname_)
+	{
+	  std::cout << "  ---> " << fn << std::endl;
 
-      TFile* f = new TFile (line.c_str());
-      TH1F* h = (TH1F*) f->Get(histoname_.c_str());
+	  ifstream fList(fn);
+	  if (!fList.good())
+		{
+		  std::regex r1(".+goodfiles_resub\\d\\.txt$");
+		  std::regex r2(".+goodfiles\\.txt$");
+		  std::smatch m;
+		  if(std::regex_match(fn, m, r1) or std::regex_match(fn, m, r2)) {
+			std::string mes = "*** Sample::openFileAndTree : WARNING: File " + fn + " was not found.";
+			cerr << mes << endl;
+			continue;
+		  }
+		  else {
+			cerr << "*** Sample::openFileAndTree : ERROR : could not open file " << fn << endl;
+			return false;
+		  }
+		}
+	  string line;
+	  while (std::getline(fList, line))
+		{
+		  line = line.substr(0, line.find("#", 0)); // remove comments introduced by #
+		  while (line.find(" ") != std::string::npos)
+			line = line.erase(line.find(" "), 1); // remove white spaces
+		  while (line.find("\n") != std::string::npos)
+			line = line.erase(line.find("\n"), 1); // remove new line characters
+		  while (line.find("\r") != std::string::npos)
+			line = line.erase(line.find("\r"), 1); // remove carriage return characters
 
-      evt_num_  += h->GetBinContent (2) ;
-      evt_den_  += h->GetBinContent (bin_eff_den_) ;
-      nentries_ += h->GetBinContent (4) ; // NB! rounding errors could make this different from the actual entries in the tree --> better to use TH1D
-      delete h;
-      delete f;
-    }
-  }
+		  if (!line.empty()) // skip empty lines
+			{
+			  ++counter;
+			  tree_->Add(line.c_str());
+
+			  TFile* f = new TFile (line.c_str());
+			  if(f->IsZombie())
+				throw std::runtime_error("File " + line + " has issues!");
+
+			  TH1F* h = (TH1F*) f->Get(histoname_.c_str());
+			  if(!h)
+				throw std::runtime_error("Histogram " + histoname_ + " not found!");
+
+			  evt_num_  += h->GetBinContent(2) ;
+			  evt_den_  += h->GetBinContent(bin_eff_den_) ;
+			  nentries_ += h->GetBinContent(4) ; // NB! rounding errors could make this different from the actual entries in the tree --> better to use TH1D
+			  delete h;
+			  delete f;
+			}
+		}
+	}
   eff_ = evt_num_ / evt_den_ ;
-  cout << "  ---> read " << counter << " files, " << nentries_ << " events" << endl;
-  cout << "  ---> efficiency is " << eff_ << "(" << evt_num_ << "/" << evt_den_ << ")" << endl;
+  std::cout << "  ---> read " << counter << " files, " << nentries_ << " events" << std::endl;
+  std::cout << "  ---> efficiency is " << eff_ << "(" << evt_num_ << "/" << evt_den_ << ")" << std::endl;
 
   return true;
-  // fIn_ = TFile::Open(filename.c_str());
-
-  // if (!fIn_)
-  // {
-  //     cerr << "*** Sample: error: could not open file " << filename << endl;
-  //     return;
-  // }
-  // if (!fIn_->GetListOfKeys()->Contains(treename.c_str()))
-  // {
-  //     cerr << "*** Sample: error: file does not contain tree " << treename << endl;
-  //     return;
-  // }
-
-  // tree_ = (TTree*) fIn_->Get(treename.c_str());
-  // nentries_ = tree_->GetEntries();
-  // cout << "  ---> success, tree contains " << nentries_ << " entries" << endl;
 }
-
+ 
 void Sample::scaleAll(double scale)
 {
   // 1D
