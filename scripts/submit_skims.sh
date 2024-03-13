886adc44666590bc06be121ab1a1a296a79c02fb
Bruno Alves <balves16@hotmail.com>
2024-03-13 22:31:40 +0100

    Remove VBF signals.

--- scripts/submit_skims.sh
+++ scripts/submit_skims.sh
@@ -305,7 +305,7 @@ eval `scram unsetenv -sh` # unset CMSSW environment
 declare -a LISTS_SIG=( $(/usr/bin/gfal-ls -lH ${LIST_SIG_DIR} | awk '{{printf $9" "}}') )
 cmsenv # set CMSSW environment
 
-DATA_LIST=( "GluGluToRad" "GluGluToBulkGrav" "VBFToRad" "VBFToBulkGrav" )
+DATA_LIST=( "GluGluToRad" "GluGluToBulkGrav" )
 MASSES=("250" "260" "270" "280" "300" "320" "350" "400" "450" "500" "550" "600" "650" "700" "750" "800" "850" "900" "1000" "1250" "1500" "1750" "2000" "2500" "3000")
 for ds in ${DATA_LIST[@]}; do
 	for mass in ${MASSES[@]}; do
