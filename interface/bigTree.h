8ac7bdd81dd8ab02b61575ebc5c6f48f9dc39cfb
Tobias Kramer <tobias.kramer@desy.de>
2025-01-07 14:26:10 +0100

    Apply PFJetID to AK8 Puppi jets

--- interface/bigTree.h
+++ interface/bigTree.h
@@ -262,6 +262,7 @@ class bigTree {
   std::vector<float>   *bParticleNetAK4JetTags_probg;
   std::vector<float>   *pfCombinedMVAV2BJetTags;
   std::vector<int>     *PFjetID;
+  std::vector<int>     *PFjetID_AK8Puppi;
   std::vector<float>   *jetRawf;
   std::vector<float>   *jets_JER;
 
@@ -657,6 +658,7 @@ class bigTree {
   TBranch        *b_bParticleNetAK4JetTags_probg;
   TBranch        *b_pfCombinedMVAV2BJetTags; //!
   TBranch        *b_PFjetID;   //!
+  TBranch        *b_PFjetID_AK8Puppi;   //!
   TBranch        *b_jetRawf;   //!
   TBranch        *b_jets_JER ; //!
   TBranch        *b_ak8jets_px;
@@ -922,6 +924,7 @@ class bigTree {
     bParticleNetAK4JetTags_probg = 0;
     pfCombinedMVAV2BJetTags = 0;
     PFjetID = 0;
+    PFjetID_AK8Puppi = 0;
     jetRawf = 0;
     jets_JER = 0;
     ak8jets_px = 0;
@@ -1139,6 +1142,7 @@ class bigTree {
     fChain->SetBranchAddress("bParticleNetAK4JetTags_probg", &bParticleNetAK4JetTags_probg, &b_bParticleNetAK4JetTags_probg);
     fChain->SetBranchAddress("pfCombinedMVAV2BJetTags", &pfCombinedMVAV2BJetTags, &b_pfCombinedMVAV2BJetTags);
     fChain->SetBranchAddress("PFjetID", &PFjetID, &b_PFjetID);
+    fChain->SetBranchAddress("PFjetID_AK8Puppi", &PFjetID_AK8Puppi, &b_PFjetID_AK8Puppi);
     fChain->SetBranchAddress("jetRawf", &jetRawf, &b_jetRawf);
     fChain->SetBranchAddress("jets_JER", &jets_JER, &b_jets_JER);
     fChain->SetBranchAddress("ak8jets_px",                                 &ak8jets_px, &b_ak8jets_px);
